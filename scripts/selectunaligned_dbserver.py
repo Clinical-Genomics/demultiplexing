cf1017f0f45dee7efe58e197c33bd9b7d11afca7
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-05 12:26:02 +0100

    Update selectunaligned_dbserver.py
    for test database use

--- scripts/selectunaligned_dbserver.py
+++ scripts/selectunaligned_dbserver.py
@@ -17,7 +17,7 @@ with open("/home/hiseq.clinical/.scilifelabrc", "r") as confs:
       params[pv[0]] = pv[1]
 
 now = time.strftime('%Y-%m-%d %H:%M:%S')
-cnx = mysql.connect(user=params['CLINICALDBUSER'], port=int(params['CLINICALDBPORT']), host=params['CLINICALDBHOST'], passwd=params['CLINICALDBPASSWD'], db='clinstatsdb')
+cnx = mysql.connect(user=params['CLINICALDBUSER'], port=int(params['CLINICALDBPORT']), host=params['CLINICALDBHOST'], passwd=params['CLINICALDBPASSWD'], db='csdb_test')
 cursor = cnx.cursor()
 
 # SELECT stats
@@ -28,7 +28,7 @@ FROM sample, flowcell, unaligned, project
 WHERE sample.sample_id     = unaligned.sample_id
 AND   flowcell.flowcell_id = unaligned.flowcell_id
 AND   sample.project_id    = project.project_id 
-AND   project.projectname    =  %s
+AND   project.projectname   =  %s
 AND   flowcell.flowcellname = %s
 GROUP BY samplename, flowcell.flowcell_id
 ORDER BY lane, sample.samplename, flowcellname """, (proje, flowc, ))
