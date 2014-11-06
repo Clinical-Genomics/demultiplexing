b3dfde26b50d704c2544106217993047f855afd5
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-06 13:10:00 +0100

    Update parseunaligned_dbserver.py

--- scripts/parseunaligned_dbserver.py
+++ scripts/parseunaligned_dbserver.py
@@ -60,9 +60,9 @@ else:
   sys.exit("Incorrect DB, version not found.")
 
 if (major == _MAJOR_ and minor == _MINOR_ and patch == _PATCH_):
-  print "Correct database "+_MAJOR_+"."+_MINOR_+"."+_PATCH_
+  print "Correct database "+str(_MAJOR_)+"."+str(_MINOR_)+"."+str(_PATCH_)
 else:
-  exit("Incorrect DB, version not found %s.%s.%s", _MAJOR_, _MINOR_, _PATCH_)
+  exit "Incorrect DB version. This script is made for "+str(_MAJOR_)+"."+str(_MINOR_)+"."+str(_PATCH_)+" not for "+str(major)+"."+str(minor)+"."+str(patch)
 
 sys.exit("hejda")
 
