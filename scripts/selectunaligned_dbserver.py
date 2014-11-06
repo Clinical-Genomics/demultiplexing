2e826beda0775b68ee954aa8a26422144d711f31
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-06 14:02:29 +0100

    Update selectunaligned_dbserver.py

--- scripts/selectunaligned_dbserver.py
+++ scripts/selectunaligned_dbserver.py
@@ -13,13 +13,28 @@ _MAJOR_ = 1
 _MINOR_ = 0
 _PATCH_ = 0
 
+if (len(sys.argv)>1):
+  basedir = sys.argv[1]
+else:
+  message = ("usage: "+sys.argv[0]+" <project> <flowcell> <config_file:optional>")
+  sys.exit(message)
+
+configfile = "/home/hiseq.clinical/.scilifelabrc"
+if (len(sys.argv)>3):
+  if os.path.isfile(sys.argv[3]):
+    configfile = sys.argv[3]
+    
 params = {}
-with open("/home/hiseq.clinical/.scilifelabrc", "r") as confs:
+with open(configfile, "r") as confs:
   for line in confs:
     if len(line) > 5 and not line[0] == "#":
       line = line.rstrip()
       pv = line.split(" ")
       params[pv[0]] = pv[1]
+
+# config file test
+sys.exit(configfile+ params['STATSDB'])
+
 
 now = time.strftime('%Y-%m-%d %H:%M:%S')
 cnx = mysql.connect(user=params['CLINICALDBUSER'], port=int(params['CLINICALDBPORT']), host=params['CLINICALDBHOST'], 
