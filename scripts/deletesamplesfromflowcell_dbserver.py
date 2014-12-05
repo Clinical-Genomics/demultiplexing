e869aca70845403b351f55436d8468be61087475
Rikard <rikard.erlandsson@scilifelab.se>
2014-12-05 15:40:58 +0100

    Update deletesamplesfromflowcell_dbserver.py

--- scripts/deletesamplesfromflowcell_dbserver.py
+++ scripts/deletesamplesfromflowcell_dbserver.py
@@ -35,6 +35,9 @@ with open(configfile, "r") as confs:
 sys.exit(configfile+ params['STATSDB'])
 
 now = time.strftime('%Y-%m-%d %H:%M:%S')
+# this script is written for database version:
+_VERSION_ = params['DBVERSION']
+
 cnx = mysql.connect(user=params['CLINICALDBUSER'], port=int(params['CLINICALDBPORT']), host=params['CLINICALDBHOST'], 
                     passwd=params['CLINICALDBPASSWD'], db=params['STATSDB'])
 cursor = cnx.cursor()
@@ -47,10 +50,10 @@ if row is not None:
   patch = row[2]
 else:
   sys.exit("Incorrect DB, version not found.")
-if (major == _MAJOR_ and minor == _MINOR_ and patch == _PATCH_):
-  print "Correct database "+str(_MAJOR_)+"."+str(_MINOR_)+"."+str(_PATCH_)
+if (str(major)+"."+str(minor)+"."+str(patch) == _VERSION_):
+  print "Correct database "+str(_VERSION_)
 else:
-  exit ("Incorrect DB version. This script is made for "+str(_MAJOR_)+"."+str(_MINOR_)+"."+str(_PATCH_)+
+  exit ("Incorrect DB version. This script is made for "+str(_VERSION_)+
         " not for "+str(major)+"."+str(minor)+"."+str(patch))
 
 print ("F: "+fcname)
