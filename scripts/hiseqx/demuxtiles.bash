28a44b6df70c88e8141894e9443964259bc8338a
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-03-06 16:12:18 +0100

    Succesfully demuxd Xten

--- scripts/hiseqx/demuxtiles.bash
+++ scripts/hiseqx/demuxtiles.bash
@@ -5,23 +5,23 @@
 # PARAMS #
 ##########
 
-RUNNAME=$1
+RUNDIR=$1
 
 ########
 # MAIN #
 ########
 
 NOW=$(date +"%Y%m%d%H%M%S")
-echo [${NOW}] starting overall process
+echo "[${NOW}] starting overall process"
 lanes=(1 2 3 4 5 6 7 8)
 tiles=('11 12' '21 22')
 for lane in "${lanes[@]}"; do
   for tile in "${tiles[@]}"; do
     NOW=$(date +"%Y%m%d%H%M%S")
-    echo [${NOW}] starting lane ${lane} tile ${tile}
-    sbatch demuxtiles.batch $RUNNAME /mnt/hds2/proj/bioinfo/DEMUX/$(basename $RUNNAME) ${lane} ${tile}
-    # wait a minute so copying doesn't slow the server
-    sleep 60 
+    echo "[${NOW}] starting lane ${lane} tile ${tile}"
+    sbatch demuxtiles.batch $RUNDIR /mnt/hds2/proj/bioinfo/TESTDEMUX/$(basename $RUNDIR) ${lane} ${tile}
+    # give the server some slack
+    sleep 120 
   done
 done
 NOW=$(date +"%Y%m%d%H%M%S")
