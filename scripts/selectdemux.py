bcf72f56c44d21129f5932ae41cd9b0e8bc86e39
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-16 12:08:12 +0100

    Update selectdemux.py

--- scripts/selectdemux.py
+++ scripts/selectdemux.py
@@ -13,7 +13,7 @@ pars = db.readconfig(configfile)
 
 with db.create_tunnel(pars['TUNNELCMD']):
 
-  with dbconnect(pars['CLINICALDBHOST'], pars['CLINICALDBPORT'], pars['STATSDB'], 
+  with db.dbconnect(pars['CLINICALDBHOST'], pars['CLINICALDBPORT'], pars['STATSDB'], 
                         pars['CLINICALDBUSER'], pars['CLINICALDBPASSWD']) as dbc:
 
     ver = dbc.versioncheck(pars['STATSDB'], pars['DBVERSION'])
