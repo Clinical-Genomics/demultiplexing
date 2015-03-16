7125c8cb0d7e45bbeef36df38d2662b73d95954c
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-16 12:27:27 +0100

    Update selectdemux.py

--- scripts/selectdemux.py
+++ scripts/selectdemux.py
@@ -5,8 +5,8 @@ import datetime
 import os
 from access import db
 
-if (len(sys.argv)>1):
-  configfile = sys.argv[1]
+if (len(sys.argv)>3):
+  configfile = sys.argv[3]
 else:
   configfile = 'None'
 pars = db.readconfig(configfile)
@@ -23,3 +23,20 @@ with db.create_tunnel(pars['TUNNELCMD']):
       exit(0) 
     else:
       print "Correct db " + pars['STATSDB'] + " v:" + pars['DBVERSION']
+
+    proje = sys.argv[1]
+    flowc = sys.argv[2]
+    query = (""" SELECT sample.samplename, flowcell.flowcellname, GROUP_CONCAT(unaligned.lane ORDER BY unaligned.lane), 
+    GROUP_CONCAT(unaligned.readcounts ORDER BY unaligned.lane), SUM(unaligned.readcounts), 
+    GROUP_CONCAT(unaligned.yield_mb ORDER BY unaligned.lane), SUM(unaligned.yield_mb), 
+    GROUP_CONCAT(TRUNCATE(q30_bases_pct,2) ORDER BY unaligned.lane), 
+    GROUP_CONCAT(TRUNCATE(mean_quality_score,2) ORDER BY unaligned.lane) 
+    FROM sample, flowcell, unaligned, project, demux WHERE sample.sample_id = unaligned.sample_id 
+    AND flowcell.flowcell_id = demux.flowcell_id AND unaligned.demux_id = demux.demux_id 
+    AND sample.project_id = project.project_id AND project.projectname = %s AND flowcell.flowcellname = %s 
+    GROUP BY samplename, flowcell.flowcell_id ORDER BY lane, sample.samplename, flowcellname; """, (proje, flowc, ))
+    hits = dbc.generalquery(query)
+    print "sample\tFlowcell\tLanes\treadcounts/lane\tsum_readcounts\tyieldMB/lane\tsum_yield\t%Q30\tMeanQscore"
+    for hit in hits:
+      print (hit[0] + "\t" + hit[1] + "\t" + hit[2] + "\t" + hit[3] + "\t" + str(hit[4]) + "\t" + str(hit[5]) + "\t" + 
+             str(hit[6]) + "\t" + str(hit[7]) + "\t" + str(hit[8]))
