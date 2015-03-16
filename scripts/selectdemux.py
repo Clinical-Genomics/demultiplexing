e464d6de42d237c084b7dd3dd0b112b895bffb19
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-16 10:46:14 +0100

    Create selectdemux.py

---
+++ scripts/selectdemux.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+import sys
+import datetime
+import os
+from dbaccess import *
+
+if (len(sys.argv)>1):
+  configfile = sys.argv[1]
+else:
+  configfile = 'None'
+pars = readconfig(configfile)
+
+if not os.path.isdir(pars['BACKUPCOPYFOLDER']):
+  sys.exit("No directory " + pars['BACKUPCOPYFOLDER'])
+
+with create_tunnel(pars['TUNNELCMD']):
+
+  with dbconnect(pars['CLINICALDBHOST'], pars['CLINICALDBPORT'], pars['STATSDB'], 
+                        pars['CLINICALDBUSER'], pars['CLINICALDBPASSWD']) as dbc:
+
+    ver = dbc.versioncheck(pars['STATSDB'], pars['DBVERSION'])
+
+    if not ver == 'True':
+      print "Wrong db " + pars['STATSDB'] + " v:" + ver
+      exit(0) 
+    else:
+      print "Correct db " + pars['STATSDB'] + " v:" + ver
