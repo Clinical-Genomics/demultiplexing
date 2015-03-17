9ffe4b11e73db8eb67051d22138fc0a862714378
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-17 13:05:26 +0100

    Update parsedemux.py

--- scripts/parsedemux.py
+++ scripts/parsedemux.py
@@ -208,8 +208,8 @@ with db.create_tunnel(pars['TUNNELCMD']):
 
     """ Set up data for table demux """
     
-    getdemuxquery = """ SELECT demux_id FROM demux WHERE flowcell_id = '""" + flowcellid + """' 
-                        AND datasource_id = '""" + datasourceid + """' AND basemask = '""" + bmask + """' """
+    getdemuxquery = """ SELECT demux_id FROM demux WHERE flowcell_id = '""" + str(flowcellid) + """' 
+                        AND datasource_id = '""" + str(datasourceid) + """' AND basemask = '""" + bmask + """' """
     inbddemux = dbc.generalquery(getdemuxquery)
     if not indbdemux:
       print "Demux not yet added"
