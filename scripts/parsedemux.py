17faab5d03b073f2a7e2eff4abf1528f98df14b7
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-17 15:06:00 +0100

    Update parsedemux.py

--- scripts/parsedemux.py
+++ scripts/parsedemux.py
@@ -296,6 +296,7 @@ with db.create_tunnel(pars['TUNNELCMD']):
       else:
         thehit = indbunal[0]
         unalignedid = thehit['unaligned_id']
+        print thehit, unaligned
       print "Unaligned stats for sample " + samplename + " exists in DB with unaligned_id: " + str(unalignedid)
 
 
