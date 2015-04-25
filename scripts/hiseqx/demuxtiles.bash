76ccf1720bb1c83d927030d86863518ae63bd070
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-04-25 22:59:11 +0200

    Add copy waiting in demuxtiles.*

--- scripts/hiseqx/demuxtiles.bash
+++ scripts/hiseqx/demuxtiles.bash
@@ -15,14 +15,20 @@ NOW=$(date +"%Y%m%d%H%M%S")
 echo "[${NOW}] starting overall process"
 lanes=(1 2 3 4 5 6 7 8)
 tiles=('11 12' '21 22')
+mkdir -p ${RUNDIR}/copycomplete/
 for lane in "${lanes[@]}"; do
   for tile in "${tiles[@]}"; do
     NOW=$(date +"%Y%m%d%H%M%S")
     echo "[${NOW}] starting lane ${lane} tile ${tile}"
     sbatch demuxtiles.batch $RUNDIR /mnt/hds2/proj/bioinfo/DEMUX/$(basename $RUNDIR) ${lane} ${tile}
-    # give the server some slack
-    sleep 120 
+    # Wait until the copy is complete ...
+    tile_qs=( ${tile} )
+    echo "${RUNDIR}/copycomplete/l${lane}t${tile_qs[0]}"
+    while [[ ! -e ${RUNDIR}/copycomplete/l${lane}t${tile_qs[0]} ]]; do
+        sleep 10 
+    done
   done
 done
+rm ${RUNDIR}/copycomplete/*
 NOW=$(date +"%Y%m%d%H%M%S")
 echo [${NOW}] everything started
