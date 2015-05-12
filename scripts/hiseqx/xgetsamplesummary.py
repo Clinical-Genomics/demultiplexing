b2e3641a78ad618e464099a5255fdc45a330fa58
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-05-12 11:24:05 +0200

    Bump version: 3.0.1 → 3.1.0

--- scripts/hiseqx/xgetsamplesummary.py
+++ scripts/hiseqx/xgetsamplesummary.py
@@ -8,7 +8,7 @@ import re
 import os
 from access import db, lims
 
-__version__ = '3.0.1'
+__version__ = '3.1.0'
 
 def getsamplesfromflowcell(demuxdir, flwc):
   samples = glob.glob("{demuxdir}*{flowcell}*/l?t??/Project_*/Sample_*".\
