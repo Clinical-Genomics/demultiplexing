78efa04d731f6db07dbe81f128dab8a71d3702b6
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-07 11:04:06 +0200

    Fix err in x link creation

--- scripts/hiseqx/xcreatefastqlinks.py
+++ scripts/hiseqx/xcreatefastqlinks.py
@@ -33,7 +33,7 @@ def get_indexes(demuxdir, flwc):
 
       # ADM1003A4_dual90
       indexes[ line[2].split('_')[0] ] = line[4] # only take the sane sample name
-    
+
   return indexes
 
 def make_link(demuxdir, outputdir, family_id, cust_name, sample_name, fc, index):
@@ -45,12 +45,11 @@ def make_link(demuxdir, outputdir, family_id, cust_name, sample_name, fc, index)
         "{demuxdir}*{fc}*/l?t??/Project_*/Sample_{sample_name}[BF]_*/*fastq.gz".format(
           demuxdir=demuxdir, fc=fc, sample_name=sample_name
         )))
-  
+
     for fastqfile in fastqfiles:
-	print('Creating link for {}'.format(fastqfile))
         nameparts = fastqfile.split("/")[-1].split("_")
         rundir = fastqfile.split("/")[6]
-	tile = fastqfile.split("/")[7].split('t')[1]
+        tile = fastqfile.split("/")[7].split('t')[1]
         date = rundir.split("_")[0]
         fc = rundir[-9:] + '-' + tile
         newname = "{lane}_{date}_{fc}_{sample_name}_{index}_{readdirection}.fastq.gz".format(
@@ -61,19 +60,27 @@ def make_link(demuxdir, outputdir, family_id, cust_name, sample_name, fc, index)
           index=index,
           readdirection=nameparts[3][-1:]
         )
-  
+
         if cust_name != None and family_id != None:
+            dest_fastqfile = os.path.join(os.path.join(outputdir, cust_name, family_id, 'genomes', sample_name, 'fastq', newname))
             try:
+                os.remove(dest_fastqfile)
+            except OSError:
+                pass
+            try:
+                print("Creating symlink for {} in {}".format(fastqfile, dest_fastqfile), end='')
                 os.symlink(fastqfile, os.path.join(os.path.join(outputdir, cust_name, family_id, 'genomes', sample_name, 'fastq', newname)))
             except:
-                print("Can't create symlink for {} in {}".format(sample_name, os.path.join(os.path.join(outputdir, cust_name, family_id, 'genomes', sample_name, 'fastq', newname))))
+                print(" - FAIL")
+            else:
+                print(" - SUCCESS")
 
 def main(argv):
 
   print(__version__)
 
   outputdir = '/mnt/hds/proj/bioinfo/MIP_ANALYSIS/'
-  
+
   fc = None
   if len(argv) > 0:
     try:
@@ -100,7 +107,10 @@ def main(argv):
       readcounts = .75 * float(analysistype[-3:])    # Accepted readcount is 75% of ordered million reads
       family_id = lmc.getattribute('samples', sample, 'familyID')
       cust_name = lmc.getattribute('samples', sample, 'customer')
-      if not re.match(r'cust\d{3}', cust_name):
+
+      if cust_name is not None:
+          cust_name = cust_name.lower()
+      if cust_name is None or not re.match(r'cust\d{3}', cust_name):
         print("WARNING '{}' does not match an internal customer name".format(cust_name))
         cust_name = None
       if cust_name == None:
