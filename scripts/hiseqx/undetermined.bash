56393fe37ff97fe0e66df8a850731892d616450e
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-10 12:42:55 +0200

    Fix undetermined

--- scripts/hiseqx/undetermined.bash
+++ scripts/hiseqx/undetermined.bash
@@ -5,8 +5,8 @@ for RUNDIR in /mnt/hds/proj/bioinfo/DEMUX/*CCXX; do
     for LANE in 1 2 3 4 5 6 7 8; do
         ls -l ${RUNDIR}/l${LANE}t*/Project*/Sample_*/*.fastq.gz > $RUNDIR/${LANE}.index.stats.txt
 
-        echo "sbatch -A prod001 -t 00:10:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
-        sbatch -A prod001 -t 00:10:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
+        echo "sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
+        sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
     done;
 done
 
@@ -15,7 +15,7 @@ for RUNDIR in /mnt/hds2/proj/bioinfo/DEMUX/*CCXX; do
     for LANE in 1 2 3 4 5 6 7 8; do
         ls -l ${RUNDIR}/l${LANE}t*/*/*/*.fastq.gz > $RUNDIR/${LANE}.index.stats.txt
 
-        echo "sbatch -A prod001 -t 00:10:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
-        sbatch -A prod001 -t 00:10:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
+        echo "sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
+        sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
     done;
 done
