1797f7fceff133153190c5f9c85d5889acd5593f
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-14 22:51:15 +0200

    Fix typo in xundetermined.bash

--- scripts/hiseqx/xundetermined.bash
+++ scripts/hiseqx/xundetermined.bash
@@ -8,5 +8,5 @@ for LANE in 1 2 3 4 5 6 7 8; do
     ls -l ${RUNDIR}/l${LANE}t*/*/*/Und*R1*.fastq.gz > $RUNDIR/${LANE}.index.stats.txt
 
     echo "sbatch -A prod001 -t 10:00:00 -c 1 --mail-user=kenny.billiau@scilifelab.se --mail-type=END -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
-    sbatch -A prod001 -t 10:00:00 -c 1 --mail-user=kenny.billiau@scilifelab.se --mail-type=END -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
+    sbatch -A prod001 -t 10:00:00 -c 1 --mail-user=kenny.billiau@scilifelab.se --mail-type=END -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out xundetermined.batch $RUNDIR $LANE
 done;
