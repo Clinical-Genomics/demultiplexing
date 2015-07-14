2e20ed9e9eb8848566715f03e0509d0c885d6a09
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-14 20:43:47 +0200

    Add X undetermined stats calculation

--- scripts/hiseqx/undetermined.bash
+++ scripts/hiseqx/undetermined.bash
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-for RUNDIR in /mnt/hds/proj/bioinfo/DEMUX/*CCXX; do
-    echo $RUNDIR;
-    for LANE in 1 2 3 4 5 6 7 8; do
-        ls -l ${RUNDIR}/l${LANE}t*/Project*/Sample_*/*.fastq.gz > $RUNDIR/${LANE}.index.stats.txt
+RUNDIR=$1
+echo $RUNDIR;
+for LANE in 1 2 3 4 5 6 7 8; do
+    ls -l ${RUNDIR}/l${LANE}t*/*/*/Und*R1*.fastq.gz > $RUNDIR/${LANE}.index.stats.txt
 
-        echo "sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
-        sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
-    done;
-done
-
-for RUNDIR in /mnt/hds2/proj/bioinfo/DEMUX/*CCXX; do
-    echo $RUNDIR;
-    for LANE in 1 2 3 4 5 6 7 8; do
-        ls -l ${RUNDIR}/l${LANE}t*/*/*/*.fastq.gz > $RUNDIR/${LANE}.index.stats.txt
-
-        echo "sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
-        sbatch -A prod001 -t 10:00:00 -c 1 -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
-    done;
-done
+    echo "sbatch -A prod001 -t 10:00:00 -c 1 --mail-user=kenny.billiau@scilifelab.se --mail-type=END -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE"
+    sbatch -A prod001 -t 10:00:00 -c 1 --mail-user=kenny.billiau@scilifelab.se --mail-type=END -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out undetermined.batch $RUNDIR $LANE
+done;
