07708012fd708fb25887306fc7d3a7e148a13719
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-23 23:44:38 +0200

    Fix delivery to OUTBOX for X10

--- scripts/hiseqx/xgetfastq.bash
+++ scripts/hiseqx/xgetfastq.bash
@@ -24,8 +24,7 @@ for TILE in `ls -d ${INDIR}/*/`; do
         if [[ ${PROJECT_ID} == 'Reports' || ${PROJECT_ID} == 'Stats' ]]; then
             continue
         fi
-    
-        OUTDIR_PROJECT=Project_${PROJECT_ID}
+
         for SAMPLE_ID in `ls ${INDIR}/${TILE}/${PROJECT_ID}`; do
             for FASTQ_FILE in `ls ${INDIR}/${TILE}/${PROJECT_ID}/${SAMPLE_ID}/`; do
                 if [[ ! -s ${INDIR}/${TILE}/${PROJECT_ID}/${SAMPLE_ID}/${FASTQ_FILE} ]]; then
@@ -33,20 +32,21 @@ for TILE in `ls -d ${INDIR}/*/`; do
                 fi
 
                 SANE_SAMPLE_ID=${SAMPLE_ID//_/-}
+                BASENAME_INDIR=$(basename ${INDIR})
+                FC=${BASENAME_INDIR: -9}
 
-                mkdir -p ${OUTDIR}/${OUTDIR_PROJECT}/${FC}/
+                mkdir -p ${OUTDIR}/${PROJECT_ID}/${FC}/
                 mkdir -p ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/
 
                 # 797220_S0_L004_R1_001.fastq.gz
                 FASTQ_FILENAME=${FASTQ_FILE%%.*}
-                DATE=$(echo ${INDIR} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print arr[1]}')
-                FC=$(echo ${INDIR}   | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
+                DATE=$(echo ${BASENAME_INDIR} | awk 'BEGIN {FS="_"} {print $1}')
                 FC_TILE=${FC}-${TILE:3:2}
                 LANE=$(echo ${FASTQ_FILE} | awk '{split($(NF-1),arr,"_");print substr(arr[3],4,1)}')
                 DIR=$(echo ${FASTQ_FILE} | awk '{split($(NF-1),arr,"_");print substr(arr[4],2,1)}')
                 BC=${BARCODE_OF[${SAMPLE_ID}]}
 
-                ln -s ${INDIR}/${TILE}/${PROJECT_ID}/${SAMPLE_ID}/${FASTQ_FILE} ${OUTDIR}/${OUTDIR_PROJECT}/${FC}/${LANE}_${DATE}_${FC_TILE}_${SANE_SAMPLE_ID}_${BC}_${DIR}.fastq.gz
+                ln -s ${INDIR}/${TILE}/${PROJECT_ID}/${SAMPLE_ID}/${FASTQ_FILE} ${OUTDIR}/${PROJECT_ID}/${FC}/${LANE}_${DATE}_${FC_TILE}_${SANE_SAMPLE_ID}_${BC}_${DIR}.fastq.gz
                 ln -s ${INDIR}/${TILE}/${PROJECT_ID}/${SAMPLE_ID}/${FASTQ_FILE} ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/${LANE}_${DATE}_${FC_TILE}_${SANE_SAMPLE_ID}_${BC}_${DIR}.fastq.gz
             done
         done
