363416a5051d9e747719fb426489dc87338ff0a7
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-28 23:47:15 +0200

    Fix better meta file createion for x10

--- scripts/hiseqx/xgetfastq.bash
+++ scripts/hiseqx/xgetfastq.bash
@@ -9,6 +9,7 @@ INDIR=$1
 OUTDIR=${2:-/mnt/hds/proj/bioinfo/OUTBOX/}
 MIPDIR=${3:-/mnt/hds/proj/bioinfo/MIP_ANALYSIS/genomes/}
 
+# read in the barcodes from the samplesheet
 declare -A BARCODE_OF
 while IFS=',' read -ra LINE; do
     if [[ ${#LINE[@]} -gt 2 ]]; then
@@ -17,50 +18,75 @@ while IFS=',' read -ra LINE; do
     fi
 done < $INDIR/SampleSheet.csv
 
-# second copy the actual fastq files
+# determine some vars
 BASENAME_INDIR=$(basename ${INDIR})
 DATE=$(echo ${BASENAME_INDIR} | awk 'BEGIN {FS="_"} {print $1}')
 FC=${BASENAME_INDIR: -9}
+
+# easy way to check if we wrote meta data for a sample
+# SAMPLE_PROJECT[${SANE_SAMPLE_ID}-${PROJECT_ID}]=1
 declare -A SAMPLE_PROJECT
 
+# reset previous linking and meta data creation
 for TILE in `ls -d ${INDIR}/*/`; do
-    TILE=$(echo ${TILE} | awk 'BEGIN {FS="/"} {print $(NF-1)}')
+    TILE=$(basename ${TILE})
+    for PROJECT_DIR in `ls ${INDIR}/${TILE}`; do
+        if [[ ${PROJECT_DIR} == 'Reports' || ${PROJECT_DIR} == 'Stats' ]]; then
+            continue
+        fi
+
+        echo "rm -Rf ${OUTDIR}/${PROJECT_DIR}/${FC}/"
+        rm -Rf ${OUTDIR}/${PROJECT_DIR}/${FC}/
+    done
+done
+
+# copy the actual fastq files
+for TILE in `ls -d ${INDIR}/*/`; do
+    TILE=$(basename ${TILE})
     FC_TILE=${FC}-${TILE:3:2}
     for PROJECT_DIR in `ls ${INDIR}/${TILE}`; do
         if [[ ${PROJECT_DIR} == 'Reports' || ${PROJECT_DIR} == 'Stats' ]]; then
             continue
         fi
 
-        PROJECT_ID=${PROJECT_DIR##Project_}
+        PROJECT_ID=${PROJECT_DIR##Project_} # replace left-most occurance
+        META_FILENAME=${OUTDIR}/${PROJECT_DIR}/${FC}/meta-${PROJECT_ID}-${FC}.txt 
+        mkdir -p ${OUTDIR}/${PROJECT_DIR}/${FC}/
 
         for SAMPLE_ID in `ls ${INDIR}/${TILE}/${PROJECT_DIR}`; do
             SANE_SAMPLE_ID=${SAMPLE_ID##Sample_}
             BC=${BARCODE_OF[${SANE_SAMPLE_ID}]}
-            SANE_SAMPLE_ID=${SANE_SAMPLE_ID//_/-}
+            SANE_SAMPLE_ID=${SANE_SAMPLE_ID//_/-} # replace
+
             for FASTQ_FILE in `ls ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_ID}/`; do
                 if [[ ! -s ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_ID}/${FASTQ_FILE} ]]; then
                     continue
                 fi
 
-                mkdir -p ${OUTDIR}/${PROJECT_DIR}/${FC}/
                 mkdir -p ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/
 
                 # 797220_S0_L004_R1_001.fastq.gz
-                LANE=$(echo ${FASTQ_FILE} | awk '{split($(NF-1),arr,"_");print substr(arr[3],4,1)}')
-                DIR=$(echo ${FASTQ_FILE} | awk '{split($(NF-1),arr,"_");print substr(arr[4],2,1)}')
-                SAMPLE_FILE_NAME=${LANE}_${DATE}_${FC_TILE}_${SANE_SAMPLE_ID}_${BC}_${DIR}.fastq.gz
+                LANE=$(echo ${FASTQ_FILE} | awk 'BEGIN {FS="_" } {print substr($3,4,1)}')
+                DIR=$(echo ${FASTQ_FILE} | awk 'BEGIN {FS="_"} {print substr($4,2,1)}')
+                FASTQ_SAMPLE_ID=$(echo ${FASTQ_FILE} | awk 'BEGIN {FS="_"} {print $1}')
+                if [[ ${FASTQ_SAMPLE_ID} -eq ${PROJECT_ID} ]]; then
+                    FASTQ_SAMPLE_ID=${SANE_SAMPLE_ID}
+                fi
+                SAMPLE_FILE_NAME=${LANE}_${DATE}_${FC_TILE}_${FASTQ_SAMPLE_ID}_${BC}_${DIR}.fastq.gz
 
                 # link
                 ln -s ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_ID}/${FASTQ_FILE} ${OUTDIR}/${PROJECT_DIR}/${FC}/${SAMPLE_FILE_NAME}
                 ln -s ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_ID}/${FASTQ_FILE} ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/${SAMPLE_FILE_NAME}
-            done
 
-            # write to meta file
-            if [[ ${SAMPLE_PROJECT[${SANE_SAMPLE_ID}-${PROJECT_ID}]} -ne 1 ]]; then
-                echo "${SANE_SAMPLE_ID}	${FC}	${LANE}	${BC}	${SAMPLE_FILE_NAME}	${SAMPLE_FILE_NAME%%_1.fastq.gz%_2.fastq.gz}" >> ${OUTDIR}/${PROJECT_DIR}/${FC}/meta-${PROJECT_ID}-${FC}.txt
-                SAMPLE_PROJECT[${SANE_SAMPLE_ID}-${PROJECT_ID}]=1
-            fi
+                # write to meta file
+                if [[ ${SAMPLE_PROJECT[${SANE_SAMPLE_ID}-${PROJECT_ID}]} -ne 1 ]]; then
+                    echo -n "${SANE_SAMPLE_ID}	${FC}	${LANE}	${BC}" >> ${META_FILENAME}
+                    SAMPLE_PROJECT[${SANE_SAMPLE_ID}-${PROJECT_ID}]=1
+                fi
+                echo -n "	${SAMPLE_FILE_NAME}" >> ${META_FILENAME}
+            done
         done
+        echo "" >> ${META_FILENAME}
 
         # cp stats file
         cp ${INDIR}/stats.txt ${OUTDIR}/${PROJECT_DIR}/${FC}/stats.txt
