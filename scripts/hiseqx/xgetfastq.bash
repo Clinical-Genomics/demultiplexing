472042d1a4ca78c648c4b1731980916884a4cb26
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-30 23:17:36 +0200

    Fix X10 meta file now contains all fastq filenames

--- scripts/hiseqx/xgetfastq.bash
+++ scripts/hiseqx/xgetfastq.bash
@@ -42,50 +42,52 @@ for TILE in `ls -d ${INDIR}/*/`; do
     done
 done
 
-# gather the information for the meta file
-for FASTQ_FILE_PATH in $( cd ${INDIR} && ls */Project_*/Sample_*/*fastq.gz );do
-    # get the vars from the dir parts
-    TILE=$(       echo ${FASTQ_FILE_PATH} | awk 'BEGIN {FS="/"} {print $1}')
-    PROJECT_DIR=$(echo ${FASTQ_FILE_PATH} | awk 'BEGIN {FS="/"} {print $2}')
-    SAMPLE_DIR=$( echo ${FASTQ_FILE_PATH} | awk 'BEGIN {FS="/"} {print $3}')
-    FASTQ_FILE=$( echo ${FASTQ_FILE_PATH} | awk 'BEGIN {FS="/"} {print $4}')
+# link the fastq files and create the meta file
+# loop over all projects
+for PROJECT_DIR in $(find ${INDIR} -name 'Project_*' -exec basename {} \; | uniq); do
+    PROJECT_ID=${PROJECT_DIR##Project_}  # remove prefix Project_
+    META_FILENAME=${OUTDIR}/${PROJECT_DIR}/${FC}/meta-${PROJECT_ID}-${FC}.txt 
 
-    # clean up vars
-    PROJECT_ID=${PROJECT_DIR##Project_}   # remove prefix Project_
-    SAMPLE_ID=${SAMPLE_DIR##Sample_} # remove prefix Sample_
-    BC=${BARCODE_OF[${SAMPLE_ID}]}   # look up the BarCode
-    SANE_SAMPLE_ID=${SAMPLE_ID//_/-} # replace _ with -
-
-    # get info from the fastq file name
-    # 797220_S0_L004_R1_001.fastq.gz
-    FC_TILE=${FC}-${TILE:3:2} # make sure we won't overwrite fastq files from diff tiles
-    LANE=$(echo ${FASTQ_FILE} | awk 'BEGIN {FS="_"} {print substr($3,4,1)}')
-    DIR=$( echo ${FASTQ_FILE} | awk 'BEGIN {FS="_"} {print substr($4,2,1)}')
-
-    # Include undetermined without he sample name
-    FASTQ_SAMPLE_ID=$(echo ${FASTQ_FILE} | awk 'BEGIN {FS="_"} {print $1}')
-    if [[ ${FASTQ_SAMPLE_ID} -eq ${PROJECT_ID} ]]; then
-        FASTQ_SAMPLE_ID=${SANE_SAMPLE_ID}
-    fi
-    SAMPLE_FILE_NAME=${LANE}_${DATE}_${FC_TILE}_${FASTQ_SAMPLE_ID}_${BC}_${DIR}.fastq.gz
-
-    # make some dirs :)
     mkdir -p ${OUTDIR}/${PROJECT_DIR}/${FC}/
-    mkdir -p ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/
 
-    # link
-    ln -s ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_DIR}/${FASTQ_FILE} ${OUTDIR}/${PROJECT_DIR}/${FC}/${SAMPLE_FILE_NAME}
-    ln -s ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_DIR}/${FASTQ_FILE} ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/${SAMPLE_FILE_NAME}
+    # loop over all samples of a project
+    for SAMPLE_DIR in $(find ${INDIR}/*/${PROJECT_DIR}/ -name 'Sample_*' -exec basename {} \; | uniq); do
+        SAMPLE_ID=${SAMPLE_DIR##Sample_} # remove prefix Sample_
+        BC=${BARCODE_OF[${SAMPLE_ID}]}   # look up the BarCode
+        SANE_SAMPLE_ID=${SAMPLE_ID//_/-} # replace _ with -
 
-    # cp stats file - TODO only copy the relevant lane
+        mkdir -p ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/
+
+        SAMPLE_FILE_NAMES=''
+        # loop over all fastq files of a sample
+        for FASTQ_FILE_PATH in $(cd ${INDIR} && ls */${PROJECT_DIR}/${SAMPLE_DIR}/*.fastq.gz); do
+            FASTQ_FILE=$(basename ${FASTQ_FILE_PATH})
+            FASTQ_SAMPLE_ID=$(echo ${FASTQ_FILE} | awk 'BEGIN {FS="_"} {print $1}')
+
+            # Determine if this is an Undetermined index fastq file
+            if [[ ${FASTQ_SAMPLE_ID} -eq ${PROJECT_ID} ]]; then
+                FASTQ_SAMPLE_ID=${SANE_SAMPLE_ID}
+            fi
+
+            TILE=$(echo ${FASTQ_FILE_PATH} | awk 'BEGIN {FS="/"} {print $1}')
+            DIR=$( echo ${FASTQ_FILE}      | awk 'BEGIN {FS="_"} {print substr($4,2,1)}')
+            LANE=${TILE:1:1}
+            FC_TILE=${FC}-${TILE:3:2} # make sure we won't overwrite fastq files from diff tiles
+
+            SAMPLE_FILE_NAME=${LANE}_${DATE}_${FC_TILE}_${FASTQ_SAMPLE_ID}_${BC}_${DIR}.fastq.gz
+            
+            # link
+            ln -s ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_DIR}/${FASTQ_FILE} ${OUTDIR}/${PROJECT_DIR}/${FC}/${SAMPLE_FILE_NAME}
+            ln -s ${INDIR}/${TILE}/${PROJECT_DIR}/${SAMPLE_DIR}/${FASTQ_FILE} ${MIPDIR}/${SANE_SAMPLE_ID}/fastq/${SAMPLE_FILE_NAME}
+
+            # write the fastq file to the meta file
+            SAMPLE_FILE_NAMES="${SAMPLE_FILE_NAMES}	${SAMPLE_FILE_NAME}"
+        done
+
+        # write out part of the meta file
+        echo "${SANE_SAMPLE_ID}  ${FC}   ${LANE} ${BC}${SAMPLE_FILE_NAMES}" >> ${META_FILENAME}
+    done
+
+    # cp stats file
     cp ${INDIR}/stats.txt ${OUTDIR}/${PROJECT_DIR}/${FC}/stats.txt
 done
-
-## gather some meta data information
-#PROJECT_DIRS=( )
-#for PROJECT_DIR in $(find ${INDIR} -name 'Project_*' -exec basename {} \; | uniq); do
-#    echo ${PROJECT_DIR}
-#    for FASTQ_FILE in $(cd ${INDIR} && ls */${PROJECT_DIR}/); do
-#
-#    done
-#done
