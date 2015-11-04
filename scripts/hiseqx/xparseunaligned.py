cf72b8c9eeb2b74230daa736acaf935803da18ab
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-11-04 16:03:21 +0100

    Fix typo

--- scripts/hiseqx/xparseunaligned.py
+++ scripts/hiseqx/xparseunaligned.py
@@ -285,7 +285,7 @@ def main(argv):
         flowcell = Flowcell()
         flowcell.flowcellname = flowcell_name
         flowcell.flowcell_pos = flowcell_pos
-        flowcell.hiseeqtype = 'hiseqx'
+        flowcell.hiseqtype = 'hiseqx'
         flowcell.time = func.now()
 
         SQL.add(flowcell)
