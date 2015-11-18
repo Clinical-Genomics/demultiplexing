8d2200d7fbebba2f63b729adcb151485658472b0
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-11-18 14:30:57 +0100

    Fix lc Merged and Original dirs in mergefastq

--- scripts/mergefastq.bash
+++ scripts/mergefastq.bash
@@ -13,26 +13,26 @@ DEMUX_DIR=$1
 for UNALIGN_DIR in ${DEMUX_DIR}/Unaligned*; do
     for PROJECT_DIR in ${UNALIGN_DIR}/Project_*; do
         for SAMPLE_DIR in ${PROJECT_DIR}/Sample_*; do
-            mkdir ${SAMPLE_DIR}/Merged
-            mkdir ${SAMPLE_DIR}/Original
+            mkdir ${SAMPLE_DIR}/merged
+            mkdir ${SAMPLE_DIR}/original
 
             for READ_DIR in 1 2; do
                 for LANE in $( cd ${SAMPLE_DIR} && ls *_L*fastq.gz | awk -F '_' '{ print $4 }' | sort | uniq ); do
                     OUTPUT_FILENAME=$( cd ${SAMPLE_DIR} && ls *_${LANE}_R${READ_DIR}_001* )
 
-                    echo "cat ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* > ${SAMPLE_DIR}/Merged/${OUTPUT_FILENAME}"
-                    cat ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* > ${SAMPLE_DIR}/Merged/${OUTPUT_FILENAME}
+                    echo "cat ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* > ${SAMPLE_DIR}/merged/${OUTPUT_FILENAME}"
+                    cat ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* > ${SAMPLE_DIR}/merged/${OUTPUT_FILENAME}
 
-                    echo "mv ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* ${SAMPLE_DIR}/Original/"
-                    mv ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* ${SAMPLE_DIR}/Original/
+                    echo "mv ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* ${SAMPLE_DIR}/original/"
+                    mv ${SAMPLE_DIR}/*_${LANE}_R${READ_DIR}_* ${SAMPLE_DIR}/original/
                 done
             done
 
-            echo "mv ${SAMPLE_DIR}/Merged/* ${SAMPLE_DIR}"
-            mv ${SAMPLE_DIR}/Merged/* ${SAMPLE_DIR}
+            echo "mv ${SAMPLE_DIR}/merged/* ${SAMPLE_DIR}"
+            mv ${SAMPLE_DIR}/merged/* ${SAMPLE_DIR}
 
-            echo "rmdir ${SAMPLE_DIR}/Merged/"
-            rmdir ${SAMPLE_DIR}/Merged/
+            echo "rmdir ${SAMPLE_DIR}/merged/"
+            rmdir ${SAMPLE_DIR}/merged/
         done
     done
 done
