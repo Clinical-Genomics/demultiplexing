018c8449b2e246cb52ec4455513a08e51301763a
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-12-07 10:55:34 +0100

    Bump version: 3.33.4 → 3.33.5

--- scripts/hiseqx/xundetermined.bash
+++ scripts/hiseqx/xundetermined.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=3.33.4
+VERSION=3.33.5
 echo "$0 VERSION $VERSION"
 
 RUNDIR=$1
--- scripts/sendnipt.bash
+++ scripts/sendnipt.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to send run results
 
-VERSION=3.33.4
+VERSION=3.33.5
 echo "Version $VERSION"
 
 ##########
