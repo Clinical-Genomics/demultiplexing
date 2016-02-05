04554cd32b3bd9236975fd346fb7b0645bbe45ad
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-02-05 13:10:41 +0100

    Bump version: 3.35.1 → 3.35.2

--- scripts/sendnipt.bash
+++ scripts/sendnipt.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to send run results
 
-VERSION=3.35.1
+VERSION=3.35.2
 echo "Version $VERSION"
 
 ##########
--- scripts/syncnipt.bash
+++ scripts/syncnipt.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # script to rsync a run to the NIPT server
 
-VERSION=3.35.1
+VERSION=3.35.2
 echo "Version $VERSION"
 
 ##########
