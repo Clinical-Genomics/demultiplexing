c4c6c5f28f79a97a9ef870e1fbfaa11004fec74a
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-05-03 12:48:58 +0200

    Add nipt_resend.bash

---
+++ scripts/nipt_resend.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+# script to send run results
+
+VERSION=3.38.1
+echo "Version $VERSION"
+
+##########
+# PARAMS #
+##########
+
+
+RUN_DIR=$1
+NIPTOUT=/srv/nipt_analysis_output/
+NIPTCONF=/home/clinical/.niptrc
+RUN=$(basename ${RUN_DIR})
+
+if [[ -r $NIPTCONF ]]; then
+    . $NIPTCONF
+fi
+
+#######
+# RUN #
+#######
+
+INVESTIGATOR_NAME=$(sed 's//\n/g' ${RUN_DIR}/SampleSheet.csv  | grep 'Investigator Name' - | cut -d, -f2)
+EXPERIMENT_NAME=$(sed 's//\n/g' ${RUN_DIR}/SampleSheet.csv  | grep 'Experiment Name' - | cut -d, -f2)
+INVESTIGATOR_NAME=${INVESTIGATOR_NAME%$EXPERIMENT_NAME}
+INVESTIGATOR_NAME=${INVESTIGATOR_NAME%_} # remove possible ending _
+
+TMP_OUTDIR=`mktemp -d`
+
+RESULTS_FILE_NAME=$(basename ${NIPTOUT}/{RUN}_*/*_NIPT_RESULTS.csv)
+RESULTS_FILE_NAME="${INVESTIGATOR_NAME}_NIPT_RESULTS.csv"
+
+OUTDIR=$(find ${NIPTOUT} -name "${RUN}_*" -type d)
+cp ${OUTDIR}/*_NIPT_RESULTS.csv ${TMP_OUTDIR}/${RESULTS_FILE_NAME}
+
+# FTP the results file
+NOW=$(date +"%Y%m%d%H%M%S")
+echo "lftp sftp://$NIPTSFTP_USER:$NIPTSFTP_PASSWORD@$NIPTSFTP_HOST -e 'cd SciLife_Till_StarLims; put ${TMP_OUTDIR}/${RESULTS_FILE_NAME}; bye'"
+lftp sftp://$NIPTSFTP_USER:$NIPTSFTP_PASSWORD@$NIPTSFTP_HOST -e "cd SciLife_Till_StarLims; put ${TMP_OUTDIR}/${RESULTS_FILE_NAME}; bye"
