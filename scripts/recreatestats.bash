14df64a9e74fc16b0417eb8cb51ee231494ab1f0
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-06-07 11:30:22 +0200

    Add --sr51s6 option to demux

--- scripts/recreatestats.bash
+++ scripts/recreatestats.bash
@@ -41,6 +41,8 @@ if [ $BASEMASKBYPASS ]; then
     UNALDIR=Unaligned10
   elif [ $BASEMASKBYPASS == '--sr51d8' ]; then
     UNALDIR=Unaligned11
+  elif [ $BASEMASKBYPASS == '--hos6' ]; then
+    UNALDIR=Unaligned12
   else
     >&2 echo "'$BASEMASKBYPASS' not recognized!"
     >&2 echo "Available options are:"
@@ -56,6 +58,8 @@ if [ $BASEMASKBYPASS ]; then
     >&2 echo "--hod8 High Output run with dual8 index"
     >&2 echo "--hos8d8 High Output run with single 8 index advertised as dual 8 index"
     >&2 echo "--hos6d8 High Output run with single 6 index advertised as dual 8 index"
+    >&2 echo "--hos6 High Output run with single 6 index"
+    >&2 echo "--sr51d8 Single Read run (51cycles) with dual 8 index"
   fi
 else
   
