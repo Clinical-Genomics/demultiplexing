d566a8e2fa6377774ec13d8969bd93d279afed87
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-06-17 15:30:44 +0200

    Feat mv sbatch params to sbatch scripts

--- scripts/hiseqx/xundetermined.bash
+++ scripts/hiseqx/xundetermined.bash
@@ -14,6 +14,6 @@ FC=$( basename `dirname ${RUNDIR}/SampleSheet.csv` | awk 'BEGIN {FS="/"} {split(
 for LANE in 1 2 3 4 5 6 7 8; do
     ls -l ${RUNDIR}/l${LANE}t*/Und*R1*.fastq.gz > ${LOGDIR}/${LANE}.index.stats.txt
 
-    echo "sbatch -A prod001 -t 10:00:00 -c 1 -J 'xundetermined-${FC}' --mail-user=kenny.billiau@scilifelab.se --mail-type=END -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out $SCRIPTDIR/undetermined.batch $RUNDIR $LANE"
-    sbatch -A prod001 -t 10:00:00 -c 1 -J "xundetermined-${FC}" --mail-user=kenny.billiau@scilifelab.se --mail-type=END -e /mnt/hds/proj/bioinfo/LOG/undetermined-%j.err -o /mnt/hds/proj/bioinfo/LOG/undetermined-%j.out $SCRIPTDIR/xundetermined.batch $RUNDIR $LANE
+    echo "sbatch -J 'xundetermined-${FC}' $SCRIPTDIR/undetermined.batch $RUNDIR $LANE"
+    sbatch -J "xundetermined-${FC}" $SCRIPTDIR/xundetermined.batch $RUNDIR $LANE
 done;
