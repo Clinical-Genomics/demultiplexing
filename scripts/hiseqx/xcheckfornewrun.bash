5a5e44f0a55877e6d37bd383d30c1540e2aae35d
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-06-26 23:15:24 +0200

    Fix sequential demuxing X

--- scripts/hiseqx/xcheckfornewrun.bash
+++ scripts/hiseqx/xcheckfornewrun.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -u
+
 ########
 # VARS #
 ########
@@ -23,22 +26,22 @@ for run in ${runs[@]}; do
 
   read -a RUN_PARTS <<< ${run}
 
-  FC=$( echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}') 
+  FC=$( echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
 
   if [ -f ${RAWBASE}${run}/RTAComplete.txt ]; then
     if [ ! -f ${RAWBASE}${run}/demuxstarted.txt ]; then
 
         # process FCs serially
-        FCS=$(squeue --format=%j | grep Xdem | grep -v ${FC} | cut -d- -f 3 | sort | uniq)
+        FCS=( $(squeue --format=%j | grep Xdem | grep -v ${FC} | cut -d- -f 3 | sort | uniq) )
         if [[ ${#FCS[@]} > 0 ]]; then
-            RUNNING_FCS=$(join , ${FS[@]})
-            echo [$NOW] ${run} ${RUNNING_FCS} Are demuxing - Postpone demux!
+            RUNNING_FCS=$(join , ${FCS[@]})
+            echo [$NOW] ${run} ${RUNNING_FCS} are demuxing - Postpone demux!
             continue
         fi
 
         echo [${NOW}] ${run} starting demultiplexing
         date +'%Y%m%d%H%M%S' > ${RAWBASE}${run}/demuxstarted.txt
-        #${SCRIPT_DIR}/xdemuxtiles.bash ${RAWBASE}${run}
+        ${SCRIPT_DIR}/xdemuxtiles.bash ${RAWBASE}${run}
     else
       echo [${NOW}] ${run} is finished and demultiplexing has already started
     fi
