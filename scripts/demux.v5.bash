99d004488e07324b6e6f6a74986b8f93c20119e2
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-05-12 16:28:19 +0200

    Cleaner logging!

--- scripts/demux.v5.bash
+++ scripts/demux.v5.bash
@@ -3,7 +3,7 @@
 #   The output i.e. Unaligned dir will be created 
 #   under $DEMUX_DIR
 
-set -eu
+set -eu -o pipefail
 
 VERSION=3.44.7
 
@@ -14,8 +14,11 @@ VERSION=3.44.7
 BASE=${1?'please provide a run dir'}
 EMAIL=kenny.billiau@scilifelab.se
 DEMUX_DIR=/home/clinical/DEMUX/
+DEST_SERVER=rastapopoulos
+DEST_DIR=/mnt/hds/proj/bioinfo/DEMUX/
 
 RUN=$(basename ${BASE})
+RUN_DIR=$(dirname ${BASE})
 PROJECTLOG=${DEMUX_DIR}/${RUN}/projectlog.$(date +"%Y%m%d%H%M%S").txt
 
 #############
@@ -24,7 +27,9 @@ PROJECTLOG=${DEMUX_DIR}/${RUN}/projectlog.$(date +"%Y%m%d%H%M%S").txt
 
 log() {
     NOW=$(date +"%Y%m%d%H%M%S")
-    echo "[$NOW] $@" >> ${PROJECTLOG}
+    # the weird sed command converts control chars to the string representation
+    echo "[$NOW] $@"
+    echo "[$NOW] $@" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" >> ${PROJECTLOG}
 }
 
 failed() {
@@ -36,16 +41,17 @@ trap failed ERR
 # MAIN #
 ########
 
-log "${PROJECTLOG} created by $0 $VERSION"
-
 # init
 mkdir -p ${DEMUX_DIR}/${RUN}
+log "${PROJECTLOG} created by $0 $VERSION"
 date > ${DEMUX_DIR}/${RUN}/started.txt
 
 # transform SampleSheet from Mac to Unix
 if [[ ! -e ${BASE}/SampleSheet.ori ]]; then
     cp ${BASE}/SampleSheet.csv ${BASE}/SampleSheet.ori
+    set +e
     if grep -qs $'\r' ${BASE}/SampleSheet.csv; then
+        set -e
         sed -i 's//\n/g' ${BASE}/SampleSheet.csv
     fi
     sed -i '/^$/d' ${BASE}/SampleSheet.csv # remove empty lines
@@ -55,7 +61,7 @@ fi
 # some sanity checking
 if [ -f ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv ]; then 
     fcinfile=$(awk 'BEGIN {FS=","} {fc=$1} END {print fc}' ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv)
-    runfc=$(echo ${BASE} | awk 'BEGIN {FS="_"} {print substr($4,2,9)}')
+    runfc=$(echo ${RUN} | awk 'BEGIN {FS="/"} {split($(NF),arr,"_");print substr(arr[4],2,length(arr[4]))}')
     if [ ! ${runfc} == ${fcinfile} ]; then 
         log "Wrong Flowcell ID in SampleSheet. Exit . . ."
         exit 1
@@ -100,46 +106,45 @@ fi
 
 # put the basemask together
 BASEMASK="${Y1},${I1}${I2}${Y2}"
-UNALDIR=Unaligned
+UNALDIR=Unaligned-${BASEMASK//,}
 
 # DEMUX !
 log "/usr/local/bin/configureBclToFastq.pl --force --sample-sheet ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv --ignore-missing-bcl --ignore-missing-stats --use-bases-mask ${BASEMASK} --fastq-cluster-count 0 --input-dir ${BASE}/Data/Intensities/BaseCalls --output-dir ${DEMUX_DIR}/${RUN}/${UNALDIR}"
-/usr/local/bin/configureBclToFastq.pl --force --sample-sheet ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv --ignore-missing-bcl --ignore-missing-stats --use-bases-mask ${BASEMASK} --fastq-cluster-count 0 --input-dir ${BASE}/Data/Intensities/BaseCalls --output-dir ${DEMUX_DIR}/${RUN}/${UNALDIR}
+# the sed command is there to remove the color codes out of the demux output and create a pretty log file
+/usr/local/bin/configureBclToFastq.pl --force --sample-sheet ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv --ignore-missing-bcl --ignore-missing-stats --use-bases-mask ${BASEMASK} --fastq-cluster-count 0 --input-dir ${BASE}/Data/Intensities/BaseCalls --output-dir ${DEMUX_DIR}/${RUN}/${UNALDIR} 2>&1 | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" >> ${PROJECTLOG}
 
 cd ${DEMUX_DIR}/${RUN}/${UNALDIR}
 nohup make -j 8 > nohup.${NOW}.out 2>&1
 
 # Add stats
-log "Demultiplexing finished,  adding stats to clinstatsdb . . ."
-/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/parsedemux.py /home/clinical/DEMUX/${RUN}/ ${UNALDIR}/ /home/clinical/RUNS/${RUN}/Data/Intensities/BaseCalls/SampleSheet.csv >> ${PROJECTLOG}
-/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/parsedemux.py /home/clinical/DEMUX/${RUN}/ ${UNALDIR}/ /home/clinical/RUNS/${RUN}/Data/Intensities/BaseCalls/SampleSheet.csv ~/.scilifelabrc_aws >> ${PROJECTLOG}
-log "/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/parsedemux.py /home/clinical/DEMUX/${RUN}/ ${UNALDIR}/ /home/clinical/RUNS/${RUN}/Data/Intensities/BaseCalls/SampleSheet.csv"
-log "/home/clinical/DEMUX/${RUN}/ /home/clinical/RUNS/${RUN}/Data/Intensities/BaseCalls/SampleSheet.csv"
+log "/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/parsedemux.py ${DEMUX_DIR}/${RUN}/ ${UNALDIR}/ ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv"
+/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/parsedemux.py ${DEMUX_DIR}/${RUN}/ ${UNALDIR}/ ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv >> ${PROJECTLOG}
+/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/parsedemux.py ${DEMUX_DIR}/${RUN}/ ${UNALDIR}/ ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv ~/.scilifelabrc_aws >> ${PROJECTLOG}
 
 # create stats files
-FC=$(echo ${BASE} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
+FC=$(echo ${RUN} | awk 'BEGIN {FS="/"} {split($(NF),arr,"_");print substr(arr[4],2,length(arr[4]))}')
 PROJs=$(ls ${DEMUX_DIR}/${RUN}/${UNALDIR}/ | grep Proj)
 for PROJ in ${PROJs[@]};do
     prj=$(echo ${PROJ} | sed 's/Project_//')
-    echo "/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/selectdemux.py ${prj} ${FC} >> ${DEMUX_DIR}/${RUN}/stats-${prj}-${FC}.txt" >> ${PROJECTLOG}
+    log "/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/selectdemux.py ${prj} ${FC} >> ${DEMUX_DIR}/${RUN}/stats-${prj}-${FC}.txt"
     /home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/selectdemux.py ${prj} ${FC} > ${DEMUX_DIR}/${RUN}/stats-${prj}-${FC}.txt
 done
 
 # skip NIPT runs
+set +e
 if ! grep -qs Description,cfDNAHiSeqv1.0 ${DEMUX_DIR}/${RUN}/SampleSheet.csv; then
-    log "rsync -r -t -e ssh ${DEMUX_DIR}/${RUN} rastapopoulos.scilifelab.se:/mnt/hds/proj/bioinfo/DEMUX/"
-    
-    # sync results to rasta
-    if rsync -r -t -e ssh ${DEMUX_DIR}/${RUN} rastapopoulos.scilifelab.se:/mnt/hds/proj/bioinfo/DEMUX/; then
-        log "rsync to rasta failed: Error code: $?"
-    else 
-        date > ${DEMUX_DIR}/${RUN}/copycomplete.txt
-        log "scp ${DEMUX_DIR}/${RUN}/copycomplete.txt rastapopoulos.scilifelab.se:/mnt/hds/proj/bioinfo/DEMUX/${RUN}"
-        scp ${DEMUX_DIR}/${RUN}/copycomplete.txt rastapopoulos.scilifelab.se:/mnt/hds/proj/bioinfo/DEMUX/${RUN}
-        log "ssh rastapopoulos.scilifelab.se \"chmod g+w /mnt/hds/proj/bioinfo/DEMUX/${RUN}\""
-        ssh rastapopoulos.scilifelab.se "chmod g+w /mnt/hds/proj/bioinfo/DEMUX/${RUN}"
-        log "scp ${PROJECTLOG} rastapopoulos.scilifelab.se:/mnt/hds/proj/bioinfo/DEMUX/${RUN}"
-        scp ${PROJECTLOG} rastapopoulos.scilifelab.se:/mnt/hds/proj/bioinfo/DEMUX/${RUN}
-        log "DEMUX transferred, script ends"
-    fi
+    set -e
+    log "rsync -r -t -e ssh ${DEMUX_DIR}/${RUN} ${DEST_SERVER}:${DEST_DIR}"
+    rsync -r -t -e ssh ${DEMUX_DIR}/${RUN} ${DEST_SERVER}:${DEST_DIR}
+
+    date > ${DEMUX_DIR}/${RUN}/copycomplete.txt
+
+    log "scp ${DEMUX_DIR}/${RUN}/copycomplete.txt ${DEST_SERVER}:${DEST_DIR}/${RUN}"
+    scp ${DEMUX_DIR}/${RUN}/copycomplete.txt ${DEST_SERVER}:${DEST_DIR}/${RUN}
+    log "ssh ${DEST_SERVER} "chmod g+w ${DEST_DIR}/${RUN}""
+    ssh ${DEST_SERVER} "chmod g+w ${DEST_DIR}/${RUN}"
+    log "scp ${PROJECTLOG} ${DEST_SERVER}:${DEST_DIR}/${RUN}"
+    scp ${PROJECTLOG} ${DEST_SERVER}:${DEST_DIR}/${RUN}
+
+    log "DEMUX transferred, script ends"
 fi
