7fc3d8cc3ecf7e8191b30c7c45a01d20eb7f38da
Kenny Billiau <kenny.billiau@scilifelab.se>
2017-06-02 23:56:42 +0200

    Possibly fixed detemrineing basemask on Read Number in runParamaters.xml

--- tests/test.bash
+++ tests/test.bash
@@ -15,12 +15,12 @@ NOK() {
 
 # test out basemask creation
 
-echo -n '170531_D00410_0425_AHKL7LBCXY '
+echo -n '170531_D00410_0425_AHKL7LBCXY Y101,I8,Y101 '
 [[ $(get_basemask ${TEST_DIR}/170531_D00410_0425_AHKL7LBCXY/) == 'Y101,I8,Y101' ]] && echo $(OK) || echo $(NOK)
 
-echo -n '170517_D00456_0219_AHJY2MBCXY '
+echo -n '170517_D00456_0219_AHJY2MBCXY Y51,I8,I8 '
 [[ $(get_basemask ${TEST_DIR}/170517_D00456_0219_AHJY2MBCXY/) == 'Y51,I8,I8' ]] && echo $(OK) || echo $(NOK)
 
-echo -n '170511_D00456_0216_BHKLC5BCXY '
+echo -n '170511_D00456_0216_BHKLC5BCXY Y101,I6nn,Y101 '
 [[ $(get_basemask ${TEST_DIR}/170511_D00456_0216_BHKLC5BCXY/) == 'Y101,I6nn,Y101' ]] && echo $(OK) || echo $(NOK)
 
