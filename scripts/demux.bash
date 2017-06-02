076822b6444b519398a9338e6b1a6645754485bb
Kenny Billiau <kenny.billiau@scilifelab.se>
2017-06-02 23:11:00 +0200

    Add first bash utests :)

--- scripts/demux.bash
+++ scripts/demux.bash
@@ -5,6 +5,9 @@
 
 set -eu -o pipefail
 
+shopt -s expand_aliases
+source ./demux.functions
+
 VERSION=4.0.4
 
 ##########
@@ -26,7 +29,7 @@ PROJECTLOG=${DEMUX_DIR}/${RUN}/projectlog.$(date +"%Y%m%d%H%M%S").txt
 #############
 
 log() {
-    NOW=$(date +"%Y%m%d%H%M%S")
+    local NOW=$(date +"%Y%m%d%H%M%S")
     # the weird sed command converts control chars to the string representation
     echo "[$NOW] $@"
     echo "[$NOW] $@" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" >> ${PROJECTLOG}
@@ -58,55 +61,21 @@ if [[ ! -e ${BASE}/SampleSheet.ori ]]; then
     cp ${BASE}/SampleSheet.csv ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv
 fi
 
-# some sanity checking
-if [ -f ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv ]; then 
-    fcinfile=$(awk 'BEGIN {FS=","} {fc=$1} END {print fc}' ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv)
-    runfc=$(echo ${RUN} | awk 'BEGIN {FS="/"} {split($(NF),arr,"_");print substr(arr[4],2,length(arr[4]))}')
-    if [ ! ${runfc} == ${fcinfile} ]; then 
-        log "Wrong Flowcell ID in SampleSheet. Exit . . ."
-        exit 1
-    fi
-fi
+## some sanity checking
+#if [[ -f ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv ]]; then 
+#    fcinfile=$(awk 'BEGIN {FS=","} {fc=$1} END {print fc}' ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv)
+#    runfc=$( basename ${RUN} | awk 'BEGIN {FS="_"} {print $NF}')
+#    if [[ ! ${runfc} == ${fcinfile} ]]; then 
+#        log "Wrong Flowcell ID in SampleSheet. Exit . . ."
+#        exit 1
+#    fi
+#fi
 
 # here we go!
 log "Setup correct, starts demuxing . . ."
 
-# get the index lengths
-SAMPLE_INDEX=( $(cat ${BASE}/SampleSheet.csv | sed -n "2p" | sed -e 's/,/\n/g' ) )
-SAMPLE_INDEX=( $( echo ${SAMPLE_INDEX[4]} | sed -e 's/-/\n/g' ) )
-LEN_SAMPLE_INDEX1=${#SAMPLE_INDEX[0]}
-LEN_SAMPLE_INDEX2=${#SAMPLE_INDEX[1]}
-
-# determine index length of run
-indexread1count=$(grep IndexRead1 ${BASE}/runParameters.xml | sed 's/<\/IndexRead1>\r//' | sed 's/    <IndexRead1>//')
-indexread2count=$(grep IndexRead2 ${BASE}/runParameters.xml | sed 's/<\/IndexRead2>\r//' | sed 's/    <IndexRead2>//')
-LEN_Y1=$(grep \<Read1\> ${BASE}/runParameters.xml | sed 's/<\/Read1>\r//' | sed 's/    <Read1>//')
-LEN_Y2=$(grep \<Read2\> ${BASE}/runParameters.xml | sed 's/<\/Read2>\r//' | sed 's/    <Read2>//')
-
-# determine index1 length
-# http://stackoverflow.com/a/5349772/322188
-I1n=$(head -c $(( ${indexread1count} - ${LEN_SAMPLE_INDEX1} )) < /dev/zero | tr '\0' 'n') # print 'n' n times
-I1="I${LEN_SAMPLE_INDEX1}${I1n}"
-I2=''
-
-# determine index2 length
-if [[ ${indexread2count} -gt 0 ]]; then # dual
-    if [[ ${LEN_SAMPLE_INDEX2} -gt 0 ]]; then
-        I2="I${LEN_SAMPLE_INDEX2}"
-    fi
-    I2n=$(head -c $(( ${indexread2count} - ${LEN_SAMPLE_INDEX2} )) < /dev/zero | tr '\0' 'n') # print 'n' n times
-    I2=",${I2}${I2n}"
-fi
-
-# determine read2
-Y1=Y${LEN_Y1}
-Y2=
-if [[ ${LEN_Y2} -gt 0 ]]; then
-    Y2=,Y${LEN_Y2}
-fi
-
-# put the basemask together
-BASEMASK="${Y1},${I1}${I2}${Y2}"
+echo $(get_basemask ${BASE})
+BASEMASK=$(get_basemask ${BASE})
 UNALDIR=Unaligned-${BASEMASK//,}
 
 # DEMUX !
@@ -115,7 +84,7 @@ log "/usr/local/bin/configureBclToFastq.pl --force --sample-sheet ${BASE}/Data/I
 /usr/local/bin/configureBclToFastq.pl --force --sample-sheet ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv --ignore-missing-bcl --ignore-missing-stats --use-bases-mask ${BASEMASK} --fastq-cluster-count 0 --input-dir ${BASE}/Data/Intensities/BaseCalls --output-dir ${DEMUX_DIR}/${RUN}/${UNALDIR} 2>&1 | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" >> ${PROJECTLOG}
 
 cd ${DEMUX_DIR}/${RUN}/${UNALDIR}
-nohup make -j 8 > nohup.${NOW}.out 2>&1
+nohup make -j 8 > nohup.$(date +"%Y%m%d%H%M%S").out 2>&1
 
 # Add stats
 log "/home/hiseq.clinical/.virtualenv/mysql/bin/python /home/clinical/SCRIPTS/parsedemux.py ${DEMUX_DIR}/${RUN}/ ${UNALDIR}/ ${BASE}/Data/Intensities/BaseCalls/SampleSheet.csv"
@@ -144,7 +113,7 @@ if ! grep -qs Description,cfDNAHiSeqv1.0 ${DEMUX_DIR}/${RUN}/SampleSheet.csv; th
 
     log "scp ${DEMUX_DIR}/${RUN}/copycomplete.txt ${DEST_SERVER}:${DEST_DIR}/${RUN}"
     scp ${DEMUX_DIR}/${RUN}/copycomplete.txt ${DEST_SERVER}:${DEST_DIR}/${RUN}
-    log "ssh ${DEST_SERVER} "chmod g+w ${DEST_DIR}/${RUN}""
+    log "ssh ${DEST_SERVER} 'chmod g+w ${DEST_DIR}/${RUN}'"
     ssh ${DEST_SERVER} "chmod g+w ${DEST_DIR}/${RUN}"
     log "scp ${PROJECTLOG} ${DEST_SERVER}:${DEST_DIR}/${RUN}"
     scp ${PROJECTLOG} ${DEST_SERVER}:${DEST_DIR}/${RUN}
---
+++ tests/test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+source ../scripts/demux.functions
+
+TEST_DIR=fixtures
+
+OK() {
+    echo "OK"
+}
+
+NOK() {
+    echo "NOK"
+}
+
+# test out basemask creation
+
+echo -n '170531_D00410_0425_AHKL7LBCXY '
+[[ $(get_basemask ${TEST_DIR}/170531_D00410_0425_AHKL7LBCXY/) == 'Y101,I8,Y101' ]] && echo $(OK) || echo $(NOK)
+
+echo -n '170517_D00456_0219_AHJY2MBCXY '
+[[ $(get_basemask ${TEST_DIR}/170517_D00456_0219_AHJY2MBCXY/) == 'Y51,I8,I8' ]] && echo $(OK) || echo $(NOK)
+
+echo -n '170511_D00456_0216_BHKLC5BCXY '
+[[ $(get_basemask ${TEST_DIR}/170511_D00456_0216_BHKLC5BCXY/) == 'Y101,I6nn,Y101' ]] && echo $(OK) || echo $(NOK)
+
