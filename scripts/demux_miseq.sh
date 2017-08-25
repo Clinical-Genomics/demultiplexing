5939a2268937999f12cd3a19a7d9fc567a4aca82
Kenny Billiau <kenny.billiau@scilifelab.se>
2017-08-25 20:59:32 +0200

    dd CTMR speicific hooks

--- scripts/demux_miseq.sh
+++ scripts/demux_miseq.sh
@@ -14,24 +14,25 @@ BCL2FASTA=/usr/local/bin/configureBclToFastq.pl
 
 RUNFOLDER=$1
 SAMPLESHEET=${RUNFOLDER}/SampleSheet.csv
-DEMUXSHEET=${RUNFOLDER}/deplex_sheet.csv
+DEMUXSHEET=${RUNFOLDER}/SampleSheet.csv
 
 IFS='_' read -r -a arr <<< "$RUNFOLDER"
 FCID=${arr[@]: -1:1}
 
 
 #Sanitize samplesheet
-sed -i 's/^M/\n/g; s/ //g' $SAMPLESHEET 
-
-
-awk -F, -v OFS=',' -v FCID=${FCID%/} 'BEGIN {print "FCID,Lane,SampleID,SampleRef,Index,Description,Control,Recipe,Operator,SampleProject"} \
-   {if (NR>17) print FCID, 1, $1, "hg19", $6"-"$8, $5, "N", "R1", "MS", $5 }' $SAMPLESHEET > $DEMUXSHEET
+#sed -i 's/^M/\n/g; s/ //g' $SAMPLESHEET 
+#
+#
+#awk -F, -v OFS=',' -v FCID=${FCID%/} 'BEGIN {print "FCID,Lane,SampleID,SampleRef,Index,Description,Control,Recipe,Operator,SampleProject"} \
+#   {if (NR>17) print FCID, 1, $1, "hg19", $6"-"$8, $5, "N", "R1", "MS", $5 }' $SAMPLESHEET > $DEMUXSHEET
 
 
 $BCL2FASTA --input-dir ${RUNFOLDER}/Data/Intensities/BaseCalls \
          --output-dir ${RUNFOLDER}/Unaligned \
          --sample-sheet $DEMUXSHEET \
-         --use-bases-mask Y300n,I8,I8,Y300n
+         --use-bases-mask Y300n,I8,I8,Y300n \
+         --mismatches 1
 
 cd ${RUNFOLDER}/Unaligned
 nohup make -j 8 > nohup.$(date +'%Y%m%d%H%M%S').out 2>&1
