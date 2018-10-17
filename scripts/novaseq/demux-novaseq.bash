c91d342e03959c056228ab384b16128e3b613483
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-10-17 15:38:23 +0200

    Add proper dir hiseq2500 naming

--- scripts/novaseq/demux-novaseq.bash
+++ scripts/novaseq/demux-novaseq.bash
@@ -40,11 +40,32 @@ ${BCL2FASTQ_BIN} --version
 # Here we go!
 log "Here we go!"
 
-BASEMASK=Y151,I8,I10,Y151
-UNALIGNED_DIR=Unaligned-${BASEMASK}
+BASEMASK=Y151,I8,N10,Y151
+UNALIGNED_DIR=Unaligned-${BASEMASK//,}
 
 # DEMUX !
 log "${BCL2FASTQ_BIN} --loading-threads 3 --processing-threads 12 --writing-threads 3 --runfolder-dir ${IN_DIR} --output-dir ${OUT_DIR}/${UNALIGNED_DIR} --use-bases-mask ${BASEMASK} --sample-sheet ${IN_DIR}/SampleSheet.csv"
 ${BCL2FASTQ_BIN} --loading-threads 3 --processing-threads 12 --writing-threads 3 --runfolder-dir ${IN_DIR} --output-dir ${OUT_DIR}/${UNALIGNED_DIR} --use-bases-mask ${BASEMASK} --sample-sheet ${IN_DIR}/SampleSheet.csv
+
+# Restructure the output dir!
+FC=${RUN##*_}
+FC=${FC:1}
+for PROJECT_DIR in ${OUT_DIR}/${UNALIGNED_DIR}/*; do
+    for SAMPLE_DIR in ${PROJECT_DIR}/*; do
+        for FASTQ_FILE in ${SAMPLE_DIR}/*; do
+            FASTQ=$(basename ${FASTQ_FILE})
+            log "mv ${FASTQ_FILE} ${SAMPLE_DIR}/${FC}_${FASTQ}"
+            mv ${FASTQ_FILE} ${SAMPLE_DIR}/${FC}_${FASTQ}
+        done
+
+        SAMPLE=$(basename ${SAMPLE_DIR})
+        log "mv ${SAMPLE_DIR} ${PROJECT_DIR}/Sample_${SAMPLE}"
+        mv ${SAMPLE_DIR} ${PROJECT_DIR}/Sample_${SAMPLE}
+    done
+
+    PROJECT=$(basename ${PROJECT_DIR})
+    log "mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
+    mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}
+done
 
 # Need to add stats code here :)
