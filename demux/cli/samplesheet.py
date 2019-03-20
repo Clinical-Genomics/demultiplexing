34e10b1a07660194e1b09ca69e710a200c185ba0
Barry Stokman <barry.stokman@hasta.scilifelab.se>
2019-03-20 09:19:37 +0100

    improve help and error texts: d10 -> dual 10

--- demux/cli/samplesheet.py
+++ demux/cli/samplesheet.py
@@ -68,9 +68,9 @@ def fetch(context, flowcell, application, dualindex, indexlength, longest, short
           delimiter=',', end='\n'):
     """
     Fetch a samplesheet from LIMS.
-    If a flowcell has dual indices of length 10+10 bp as well as 8+8 bp, use the option -p, or
-    --pad to add two bases to length 8 indices (AT for index1, AC for index2). This will ensure
-    that all indices in the sample sheet are of the same length.
+    If a flowcell has dual indices of length 10+10 bp (dual 10) as well as 8+8 bp (dual 8), use
+    the option -p, or --pad to add two bases to length 8 indices (AT for index1, AC for index2).
+    This will ensure that all indices in the sample sheet are of the same length.
     """
 
     def reverse_complement(dna):
@@ -159,11 +159,12 @@ def fetch(context, flowcell, application, dualindex, indexlength, longest, short
         if pad and not indexlength:
             click.echo(click.style(f"Please specify an index length when using the pad option!"
                                    f"Use --longest or --indexlength. Nota that padding only works"
-                                   f"in combination with d10 indxes!", fg='red'))
+                                   f"in combination with dual 10 indxes!", fg='red'))
             context.abort()
 
         if pad and indexlength != 20:
-            click.echo(click.style(f"Padding is only allowed in combination with d10 indexes!", fg='red'))
+            click.echo(click.style(f"Padding is only allowed in combination with dual 10 indexes!",
+                                   fg='red'))
             context.abort()
 
         lims_keys = ['fcid', 'lane', 'sample_id', 'sample_ref', 'index', 'index2', 'sample_name',
