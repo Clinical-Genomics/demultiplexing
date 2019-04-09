b18fe38c165aef37e60ab81e3cd39e347006e6e1
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-04-09 21:23:32 +0200

    Pushes novaseq demux through a bash linter
    `shellcheck *bash`

--- scripts/novaseq/checkfornewrun.bash
+++ scripts/novaseq/checkfornewrun.bash
@@ -4,7 +4,8 @@ set -u
 
 shopt -s nullglob
 shopt -s expand_aliases
-source ~/.aliases
+source "${HOME}/.bashrc"
+useprod
 
 ########
 # VARS #
@@ -12,7 +13,7 @@ source ~/.aliases
 
 IN_DIR=${1?'please provide the runs dir'}
 DEMUXES_DIR=${2?'please provide the demuxes dir'}
-SCRIPT_DIR=$(dirname $(readlink -nm $0))
+SCRIPT_DIR=$(dirname "$(readlink -nm "$0")")
 EMAIL=clinical-demux@scilifelab.se
 
 #############
@@ -20,12 +21,13 @@ EMAIL=clinical-demux@scilifelab.se
 #############
 
 log () {
+    local NOW
     NOW=$(date +"%Y%m%d%H%M%S")
-    echo [${NOW}] $@
+    echo "[${NOW}] $*"
 }
 
 failed() {
-    cat ${PROJECTLOG} | mail -s "ERROR starting novaseq ${FC} on $(hostname)" $EMAIL
+    mail -s "ERROR starting novaseq ${FC} on $(hostname)" $EMAIL < "${PROJECTLOG}"
 }
 trap failed ERR
 
@@ -38,12 +40,12 @@ if pgrep bcl2fastq; then
     exit 0
 fi
 
-for RUN_DIR in ${IN_DIR}/*; do
+for RUN_DIR in "${IN_DIR}"/*; do
     if [[ ! -d ${RUN_DIR} ]]; then
         continue
     fi
 
-    RUN=$(basename ${RUN_DIR})
+    RUN=$(basename "${RUN_DIR}")
     FC=${RUN##*_}
     FC=${FC:1}
     PROJECTLOG=${DEMUXES_DIR}/${RUN}/projectlog.$(date +'%Y%m%d%H%M%S').log
@@ -51,24 +53,23 @@ for RUN_DIR in ${IN_DIR}/*; do
     if [[ -f ${RUN_DIR}/RTAComplete.txt ]]; then
         if [[ ! -f ${RUN_DIR}/demuxstarted.txt ]]; then
             log "date +'%Y%m%d%H%M%S' > ${RUN_DIR}/demuxstarted.txt"
-            date +'%Y%m%d%H%M%S' > ${RUN_DIR}/demuxstarted.txt
+            date +'%Y%m%d%H%M%S' > "${RUN_DIR}"/demuxstarted.txt
 
             if [[ ! -e ${RUN_DIR}/SampleSheet.csv ]]; then
                 log "demux sheet fetch --application nova --pad --longest ${FC} > ${RUN_DIR}/SampleSheet.csv"
-                demux sheet fetch --application nova --pad --longest ${FC} > ${RUN_DIR}/SampleSheet.csv
+                demux sheet fetch --application nova --pad --longest "${FC}" > "${RUN_DIR}"/SampleSheet.csv
             fi
 
             log "mkdir -p ${DEMUXES_DIR}/${RUN}/"
-            mkdir -p ${DEMUXES_DIR}/${RUN}/
+            mkdir -p "${DEMUXES_DIR}/${RUN}/"
 
             log "bash ${SCRIPT_DIR}/demux-novaseq.bash ${RUN_DIR} ${DEMUXES_DIR} &>> ${PROJECTLOG}"
-            bash ${SCRIPT_DIR}/demux-novaseq.bash ${RUN_DIR} ${DEMUXES_DIR} &>> ${PROJECTLOG}
 
-            if [[ $? == 0 ]]; then
+            if bash "${SCRIPT_DIR}/demux-novaseq.bash" "${RUN_DIR}" "${DEMUXES_DIR}" &>> "${PROJECTLOG}"; then
                 log "rm -f ${DEMUXES_DIR}/${RUN}/copycomplete.txt"
-                rm -f ${DEMUXES_DIR}/${RUN}/copycomplete.txt
+                rm -f "${DEMUXES_DIR}/${RUN}/copycomplete.txt"
                 log "date +'%Y%m%d%H%M%S' > ${DEMUXES_DIR}/${RUN}/demuxcomplete.txt"
-                date +'%Y%m%d%H%M%S' > ${DEMUXES_DIR}/${RUN}/demuxcomplete.txt
+                date +'%Y%m%d%H%M%S' > "${DEMUXES_DIR}/${RUN}/demuxcomplete.txt"
             fi
         else
             log "${RUN} is finished and demultiplexing has already started"
--- scripts/novaseq/demux-novaseq.bash
+++ scripts/novaseq/demux-novaseq.bash
@@ -3,7 +3,7 @@
 set -eu
 
 shopt -s expand_aliases
-source $HOME/.bashrc
+source "$HOME/.bashrc"
 ulimit -n 4096
 
 ##########
@@ -13,7 +13,7 @@ ulimit -n 4096
 IN_DIR=${1?'please provide a run dir'}
 DEMUXES_DIR=${2?'please provide the demuxes dir'}
 
-RUN=$(basename ${IN_DIR})
+RUN=$(basename "${IN_DIR}")
 OUT_DIR=${DEMUXES_DIR}/${RUN}
 
 BCL2FASTQ_BIN=/usr/local/bcl2fastq2/bin/bcl2fastq
@@ -23,8 +23,9 @@ BCL2FASTQ_BIN=/usr/local/bcl2fastq2/bin/bcl2fastq
 #############
 
 log() {
-    local NOW=$(date +"%Y%m%d%H%M%S")
-    echo "[$NOW] $@"
+    local NOW
+    NOW=$(date +"%Y%m%d%H%M%S")
+    echo "[$NOW] $*"
 }
 
 ########
@@ -32,7 +33,7 @@ log() {
 ########
 
 # init
-mkdir -p ${OUT_DIR}
+mkdir -p "${OUT_DIR}"
 
 # log the version
 demux --version
@@ -46,49 +47,48 @@ UNALIGNED_DIR=Unaligned-${BASEMASK//,}
 
 # DEMUX !
 log "${BCL2FASTQ_BIN} --loading-threads 3 --processing-threads 12 --writing-threads 3 --runfolder-dir ${IN_DIR} --output-dir ${OUT_DIR}/${UNALIGNED_DIR} --use-bases-mask ${BASEMASK} --sample-sheet ${IN_DIR}/SampleSheet.csv --barcode-mismatches 1"
-${BCL2FASTQ_BIN} --loading-threads 3 --processing-threads 12 --writing-threads 3 --runfolder-dir ${IN_DIR} --output-dir ${OUT_DIR}/${UNALIGNED_DIR} --use-bases-mask ${BASEMASK} --sample-sheet ${IN_DIR}/SampleSheet.csv --barcode-mismatches 1
+${BCL2FASTQ_BIN} --loading-threads 3 --processing-threads 12 --writing-threads 3 --runfolder-dir "${IN_DIR}" --output-dir "${OUT_DIR}/${UNALIGNED_DIR}" --use-bases-mask ${BASEMASK} --sample-sheet "${IN_DIR}/SampleSheet.csv" --barcode-mismatches 1
 
 # add samplesheet to unaligned folder
-cp ${IN_DIR}/SampleSheet.csv ${OUT_DIR}/${UNALIGNED_DIR}/
+cp "${IN_DIR}/SampleSheet.csv" "${OUT_DIR}/${UNALIGNED_DIR}/"
 
 # Restructure the output dir!
 FC=${RUN##*_}
 FC=${FC:1}
-for PROJECT_DIR in ${OUT_DIR}/${UNALIGNED_DIR}/*; do
+for PROJECT_DIR in "${OUT_DIR}/${UNALIGNED_DIR}"/*; do
     if [[ ! -d ${PROJECT_DIR} ]]; then continue; fi
 
-    PROJECT=$(basename ${PROJECT_DIR})
+    PROJECT=$(basename "${PROJECT_DIR}")
     if [[ ${PROJECT} == 'Stats' ]]; then continue; fi
     if [[ ${PROJECT} == 'Reports' ]]; then continue; fi
     if [[ ${PROJECT} =~ Project_* ]]; then continue; fi
     if [[ ${PROJECT} == 'indexcheck' ]]; then
-        mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}
+        mv "${PROJECT_DIR}" "${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
         continue
     fi
 
-    for SAMPLE_DIR in ${PROJECT_DIR}/*; do
-        for FASTQ_FILE in ${SAMPLE_DIR}/*; do
-            FASTQ=$(basename ${FASTQ_FILE})
+    for SAMPLE_DIR in "${PROJECT_DIR}"/*; do
+        for FASTQ_FILE in "${SAMPLE_DIR}"/*; do
+            FASTQ=$(basename "${FASTQ_FILE}")
             log "mv ${FASTQ_FILE} ${SAMPLE_DIR}/${FC}_${FASTQ}"
-            mv ${FASTQ_FILE} ${SAMPLE_DIR}/${FC}_${FASTQ}
+            mv "${FASTQ_FILE}" "${SAMPLE_DIR}/${FC}_${FASTQ}"
         done
 
-        SAMPLE=$(basename ${SAMPLE_DIR})
+        SAMPLE=$(basename "${SAMPLE_DIR}")
         log "mv ${SAMPLE_DIR} ${PROJECT_DIR}/Sample_${SAMPLE}"
-        mv ${SAMPLE_DIR} ${PROJECT_DIR}/Sample_${SAMPLE}
+        mv "${SAMPLE_DIR}" "${PROJECT_DIR}/Sample_${SAMPLE}"
     done
 
     log "mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
-    mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}
+    mv "${PROJECT_DIR}" "${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
 done
 
 # Need to add stats code here :)
 log "cgstats add --machine novaseq --unaligned ${UNALIGNED_DIR} ${OUT_DIR}"
-cgstats add --machine novaseq --unaligned ${UNALIGNED_DIR} ${OUT_DIR}
+cgstats add --machine novaseq --unaligned ${UNALIGNED_DIR} "${OUT_DIR}"
 
-PROJECTS=$(ls ${OUT_DIR}/${UNALIGNED_DIR}/ | grep Project_)
-for PROJECT_DIR in ${PROJECTS[@]}; do
+for PROJECT_DIR in "${OUT_DIR}/${UNALIGNED_DIR}"/Project_*; do
     PROJECT=${PROJECT_DIR##*_}
     log "cgstats select --project ${PROJECT} ${FC} &> ${OUT_DIR}/stats-${PROJECT}-${FC}.txt"
-    cgstats select --project ${PROJECT} ${FC} &> ${OUT_DIR}/stats-${PROJECT}-${FC}.txt
+    cgstats select --project "${PROJECT}" "${FC}" &> "${OUT_DIR}/stats-${PROJECT}-${FC}.txt"
 done
