ca1302e4c0f43778fc3f99fa0a1c1e7c053cf884
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-06-18 08:44:42 +0200

    Sanitize var names

--- scripts/hiseqx/xcheckfornewrun.bash
+++ scripts/hiseqx/xcheckfornewrun.bash
@@ -8,9 +8,9 @@ set -u
 ########
 
 SCRIPT_DIR="$(dirname "$(readlink -nm "$0")")"
-RAWBASE=/mnt/hds2/proj/bioinfo/Runs/
-DEMUX_DIR=/mnt/hds/proj/bioinfo/DEMUX/
-EMAIL=clinical-demux@scilifelab.se
+RUNS_DIR=${1?please provide a runs dir} #/home/proj/${ENVIRONMENT-stage}/flowcells/hiseqx/
+DEMUXES_DIR=${2?please provide a demuxes dir} #/home/proj/${ENVIRONMENT-stage}/demultiplexed-runs/
+EMAIL=${3-clinical-demux@scilifelab.se}
 
 #############
 # FUNCTIONS #
@@ -32,12 +32,12 @@ trap failed ERR
 # MAIN #
 ########
 
-for RUN in "${RAWBASE}"/*; do
+for RUN in "${RUNS_DIR}"/*; do
     RUN="$(basename "${RUN}/")"
     FC=$( echo "${RUN}" | awk 'BEGIN {FS="_"} {print substr($4,2,length($4))}')
 
-    if [[ -f "${RAWBASE}/${RUN}/RTAComplete.txt" ]]; then
-        if [ ! -f ${RAWBASE}/${RUN}/demuxstarted.txt ]; then
+    if [[ -f "${RUNS_DIR}/${RUN}/RTAComplete.txt" ]]; then
+        if [ ! -f ${RUNS_DIR}/${RUN}/demuxstarted.txt ]; then
 
             # process FCs serially
             FCS=( $(squeue --format=%j | grep Xdem | grep -v ${FC} | cut -d- -f 3 | sort | uniq) )
@@ -48,13 +48,13 @@ for RUN in "${RAWBASE}"/*; do
             fi
 
             log "${RUN} starting demultiplexing"
-            date +'%Y%m%d%H%M%S' > ${RAWBASE}/${RUN}/demuxstarted.txt
+            date +'%Y%m%d%H%M%S' > ${RUNS_DIR}/${RUN}/demuxstarted.txt
 
-            mkdir -p ${DEMUX_DIR}/${RUN}/
-            PROJECTLOG=${DEMUX_DIR}/${RUN}/projectlog.$(date +'%Y%m%d%H%M%S').log
-            ${SCRIPT_DIR}/xdemuxtiles.bash ${RAWBASE}/${RUN} &>> ${PROJECTLOG}
-            rm -f ${DEMUX_DIR}/${RUN}/copycomplete.txt
-            rm -f ${DEMUX_DIR}/${RUN}/delivery.txt
+            mkdir -p ${DEMUXES_DIR}/${RUN}/
+            PROJECTLOG=${DEMUXES_DIR}/${RUN}/projectlog.$(date +'%Y%m%d%H%M%S').log
+            #${SCRIPT_DIR}/xdemuxtiles.bash ${RUNS_DIR}/${RUN} &>> ${PROJECTLOG}
+            rm -f ${DEMUXES_DIR}/${RUN}/copycomplete.txt
+            rm -f ${DEMUXES_DIR}/${RUN}/delivery.txt
         else
             log "${RUN} is finished and demultiplexing has already started"
         fi
