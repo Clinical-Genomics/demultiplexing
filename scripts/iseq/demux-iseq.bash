88ccbfe648012025fc5964d15401b5b5d477580a
patrikgrenfeldt <patrik.grenfeldt@scilifelab.se>
2019-10-18 11:17:25 +0200

    feat: Automatic demultiplexing of iseq runs (#91)
    * Create demux-iseq.bash
    
    * added checkfornewrun for iseq
    
    * format basemask
    
    * change novaseq to iseq
    
    * Ran everything through a linter
    
    * new iseq samplesheet (used by cgstats)
    
    * rename sample project
    
    * add flowcell id for iseq samplesheet

---
+++ scripts/iseq/demux-iseq.bash
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+set -eu
+
+shopt -s expand_aliases
+source "${HOME}/.bashrc"
+ulimit -n 4096
+
+##########
+# PARAMS #
+##########
+
+IN_DIR=${1?'please provide a run dir'}
+DEMUXES_DIR=${2?'please provide the demuxes dir'}
+
+RUN=$(basename "${IN_DIR}")
+OUT_DIR=${DEMUXES_DIR}/${RUN}
+
+BCL2FASTQ_BIN=/usr/local/bcl2fastq2/bin/bcl2fastq
+
+#############
+# FUNCTIONS #
+#############
+
+log() {
+    local NOW
+    NOW=$(date +"%Y%m%d%H%M%S")
+    echo "[$NOW] $*"
+}
+
+########
+# MAIN #
+########
+
+# init
+mkdir -p "${OUT_DIR}"
+
+# log the version
+demux --version
+${BCL2FASTQ_BIN} --version
+
+# Here we go!
+log "Here we go!"
+
+BASEMASK=Y30,I10,I10
+UNALIGNED_DIR=Unaligned-${BASEMASK//,}
+
+# DEMUX !
+log "${BCL2FASTQ_BIN} --loading-threads 3 --processing-threads 15 --writing-threads 3 --runfolder-dir ${IN_DIR} --output-dir ${OUT_DIR}/${UNALIGNED_DIR} --use-bases-mask ${BASEMASK} --sample-sheet ${IN_DIR}/SampleSheet.csv --barcode-mismatches 1"
+${BCL2FASTQ_BIN} --loading-threads 3 --processing-threads 15 --writing-threads 3 --runfolder-dir "${IN_DIR}" --output-dir "${OUT_DIR}/${UNALIGNED_DIR}" --use-bases-mask ${BASEMASK} --sample-sheet "${IN_DIR}/SampleSheet.csv" --barcode-mismatches 1
+
+# add samplesheet to unaligned folder
+cp "${IN_DIR}/SampleSheet.csv" "${OUT_DIR}/${UNALIGNED_DIR}/"
+
+# Restructure the output dir!
+FC=${RUN##*_}
+FC=${FC:1}
+shopt -s nullglob
+for PROJECT_DIR in "${OUT_DIR}/${UNALIGNED_DIR}"/*; do
+    if [[ ! -d ${PROJECT_DIR} ]]; then continue; fi
+
+    PROJECT=$(basename "${PROJECT_DIR}")
+    if [[ ${PROJECT} == 'Stats' ]]; then continue; fi
+    if [[ ${PROJECT} == 'Reports' ]]; then continue; fi
+    if [[ ${PROJECT} =~ Project_* ]]; then continue; fi
+    if [[ ${PROJECT} == 'indexcheck' ]]; then
+        mv "${PROJECT_DIR}" "${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
+        continue
+    fi
+
+    for SAMPLE_DIR in "${PROJECT_DIR}"/*; do
+        for FASTQ_FILE in "${SAMPLE_DIR}"/*; do
+            FASTQ=$(basename "${FASTQ_FILE}")
+            log "mv ${FASTQ_FILE} ${SAMPLE_DIR}/${FC}_${FASTQ}"
+            mv "${FASTQ_FILE}" "${SAMPLE_DIR}/${FC}_${FASTQ}"
+        done
+
+        SAMPLE=$(basename "${SAMPLE_DIR}")
+        log "mv ${SAMPLE_DIR} ${PROJECT_DIR}/Sample_${SAMPLE}"
+        mv "${SAMPLE_DIR}" "${PROJECT_DIR}/Sample_${SAMPLE}"
+    done
+
+    log "mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
+    mv "${PROJECT_DIR}" "${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
+done
+
+# Need to add stats code here :)
+log "cgstats add --machine iseq --unaligned ${UNALIGNED_DIR} ${OUT_DIR}"
+cgstats add --machine iseq --unaligned ${UNALIGNED_DIR} "${OUT_DIR}"
+
+PROJECTS=$(ls "${OUT_DIR}/${UNALIGNED_DIR}/" | grep Project_)
+for PROJECT_DIR in "${PROJECTS[@]}"; do
+    PROJECT=${PROJECT_DIR##*_}
+    log "cgstats select --project ${PROJECT} ${FC} &> ${OUT_DIR}/stats-${PROJECT}-${FC}.txt"
+    cgstats select --project "${PROJECT}" "${FC}" &> "${OUT_DIR}/stats-${PROJECT}-${FC}.txt"
+done
