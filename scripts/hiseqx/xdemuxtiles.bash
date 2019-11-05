7f6087e782eebcb38f66af823b1db946b90c50f3
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-11-05 12:30:33 +0100

    fix: Add qos to xdemuxtiles.bash (#93)

--- scripts/hiseqx/xdemuxtiles.bash
+++ scripts/hiseqx/xdemuxtiles.bash
@@ -111,8 +111,8 @@ log "Running ${RUNNING_JOBIDS[*]}"
 log "Demux ${DEMUX_JOBIDS[*]}"
 log "Remaining ${REMAINING_JOBIDS[*]}"
 JOB_TITLE="xdem-xpostface-${FC}"
-log "sbatch -J 'Xdem-postface' --dependency='${DEPENDENCY}' -o '${LOGDIR}/${JOB_TITLE}-%j.log' -e '${LOGDIR}/${JOB_TITLE}-%j.err' '${SCRIPTDIR}/xpostface.batch' '${OUTDIR}/'"
-     sbatch -J "Xdem-postface" --dependency="${DEPENDENCY}" -o "${LOGDIR}/${JOB_TITLE}-%j.log" -e "${LOGDIR}/${JOB_TITLE}-%j.err" "${SCRIPTDIR}/xpostface.batch" "${OUTDIR}/"
+log "sbatch -A ${SLURM_ACCOUNT} -J 'Xdem-postface' --dependency='${DEPENDENCY}' -o '${LOGDIR}/${JOB_TITLE}-%j.log' -e '${LOGDIR}/${JOB_TITLE}-%j.err' '${SCRIPTDIR}/xpostface.batch' '${OUTDIR}/'"
+     sbatch -A ${SLURM_ACCOUNT} -J "Xdem-postface" --dependency="${DEPENDENCY}" -o "${LOGDIR}/${JOB_TITLE}-%j.log" -e "${LOGDIR}/${JOB_TITLE}-%j.err" "${SCRIPTDIR}/xpostface.batch" "${OUTDIR}/"
 
 ###########
 # CLEANUP #
