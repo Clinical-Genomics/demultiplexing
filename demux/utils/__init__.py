d8729e33bf527a16a34d243037b5f590a04b3723
barrystokman <barry.stokman@gmail.com>
2020-12-04 16:02:35 +0100

    black

--- demux/utils/__init__.py
+++ demux/utils/__init__.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from .samplesheet import Samplesheet, HiSeqXSamplesheet, NIPTSamplesheet, HiSeq2500Samplesheet, \
-    MiseqSamplesheet, SampleSheetValidationException, iseqSampleSheet
+from .samplesheet import (
+    Samplesheet,
+    HiSeqXSamplesheet,
+    NIPTSamplesheet,
+    HiSeq2500Samplesheet,
+    MiseqSamplesheet,
+    SampleSheetValidationException,
+    iseqSampleSheet,
+)
+from .novaseq_samplesheet import CreateNovaseqSamplesheet
