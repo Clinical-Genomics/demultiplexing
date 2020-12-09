2656838a1147f0bc03009a5a38cb3e0f639f1b22
Barry Stokman <barry.stokman@gmail.com>
2020-12-09 16:31:34 +0100

    black

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,15 +44,19 @@ def fixture_runs_dir(novaseq_dir: Path) -> Path:
 def fixture_novaseq_runparameters_file(novaseq_runs_dir: Path) -> Path:
     """ Fixture for novaseq runparameters files """
     return {
-        "novaseq_oldSCV": novaseq_runs_dir / "RunParameters_oldSCV.xml",  #TODO: choose proper name
-        "novaseq_newSCV_oldkit": novaseq_runs_dir / "RunParameters_newSCV_oldkit.xml",  #TODO: choose proper name
-        "novaseq_newSCV_newkit": novaseq_runs_dir / "RunParameters_newSCV_newkit.xml",  #TODO: choose proper name
-        "novaseq_fluffy": novaseq_runs_dir / "RunParameters_fluffy.xml",  #TODO: choose proper name
+        "novaseq_oldSCV": novaseq_runs_dir
+        / "RunParameters_oldSCV.xml",  # TODO: choose proper name
+        "novaseq_newSCV_oldkit": novaseq_runs_dir
+        / "RunParameters_newSCV_oldkit.xml",  # TODO: choose proper name
+        "novaseq_newSCV_newkit": novaseq_runs_dir
+        / "RunParameters_newSCV_newkit.xml",  # TODO: choose proper name
+        "novaseq_fluffy": novaseq_runs_dir
+        / "RunParameters_fluffy.xml",  # TODO: choose proper name
     }
 
 
 @pytest.yield_fixture(scope="function", name="novaseq_runparameters_api")
 def fixture_novaseq_runparameters_api(novaseq_runs_dir):
     """ Set up novaseq runparameters api for testing """
-    novaseq_runparameters_api = NovaseqRunParameters('HGJJKDSXY', novaseq_runs_dir)
+    novaseq_runparameters_api = NovaseqRunParameters("HGJJKDSXY", novaseq_runs_dir)
     return novaseq_runparameters_api
