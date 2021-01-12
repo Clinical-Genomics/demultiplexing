358326a019b02062425012a8dea2b8043181e61e
Karl Nyren <kallejohan211@hotmail.com>
2021-01-12 07:08:49 +0100

    black

--- tests/utils/conftest.py
+++ tests/utils/conftest.py
@@ -7,14 +7,16 @@ from demux.utils.indexreport import IndexReport
 
 
 @pytest.fixture(name="valid_indexreport")
-def fixture_index_check_report(novaseq_valid_indexcheck_report, project_dir) -> IndexReport:
+def fixture_index_check_report(
+    novaseq_valid_indexcheck_report, project_dir
+) -> IndexReport:
     """ Fixture of a valid bcl2fastq indexcheck report """
     index_check_report = IndexReport(
         out_dir=project_dir,
         index_report_path=novaseq_valid_indexcheck_report,
         flowcell_id="HFKF7DSXY",
         cluster_counts=100000,
-        INDEX_REPORT_HEADER=INDEX_REPORT_HEADER
+        INDEX_REPORT_HEADER=INDEX_REPORT_HEADER,
     )
 
     return index_check_report
@@ -39,13 +41,15 @@ def fixture_parsed_valid_indexreport(parsed_valid_indexreport) -> IndexReport:
 
 
 @pytest.fixture(name="indexreport_wrong_header_rt1")
-def fixture_indexreport_wrong_header_rt1(novaseq_indexcheck_wrong_header_rt1, project_dir):
+def fixture_indexreport_wrong_header_rt1(
+    novaseq_indexcheck_wrong_header_rt1, project_dir
+):
     indexreport_wrong_header_rt1 = IndexReport(
         out_dir=project_dir,
         index_report_path=novaseq_indexcheck_wrong_header_rt1,
         flowcell_id="HFKF7DSXY",
         cluster_counts=100000,
-        INDEX_REPORT_HEADER=INDEX_REPORT_HEADER
+        INDEX_REPORT_HEADER=INDEX_REPORT_HEADER,
     )
     indexreport_wrong_header_rt1.parse_report()
 
