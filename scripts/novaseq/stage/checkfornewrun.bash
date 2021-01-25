0b67e2e06adce9133879905a812447640a1781cc
barrystokman <barry.stokman@gmail.com>
2021-01-25 16:00:01 +0100

    add stage ready demux scripts for testing/development

---
+++ scripts/novaseq/stage/checkfornewrun.bash
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -u
+
+shopt -s nullglob
+
+########
+# VARS #
+########
+
+IN_DIR=${1?'please provide the runs dir'}
+DEMUXES_DIR=${2?'please provide the demuxes dir'}
+SCRIPT_DIR=$(dirname $(readlink -nm $0))
+#EMAIL=clinical-demux@scilifelab.se
+EMAIL=your.name@scilifelab.se
+
+#############
+# FUNCTIONS #
+#############
+
+log () {
+    NOW=$(date +"%Y%m%d%H%M%S")
+    echo [${NOW}] $@
+}
+
+failed() {
+        cat ${PROJECTLOG} | mail -s "ERROR starting novaseq ${FC} on $(hostname), line $1" $EMAIL
+}
+trap 'failed ${LINENO}' ERR
+
+########
+# MAIN #
+########
+
+if pgrep bcl2fastq; then
+    log "an instance of bcl2fastq is already running -- skipping"
+    exit 0
+fi
+
+for RUN_DIR in ${IN_DIR}/*; do
+    if [[ ! -d ${RUN_DIR} ]]; then
+        continue
+    fi
+
+    RUN=$(basename ${RUN_DIR})
+    FC=${RUN##*_}
+    FC=${FC:1}
+    PROJECTLOG=${DEMUXES_DIR}/${RUN}/projectlog.$(date +'%Y%m%d%H%M%S').log
+
+    if [[ -f ${RUN_DIR}/RTAComplete.txt && -f ${RUN_DIR}/CopyComplete.txt ]]; then
+        if [[ ! -f ${RUN_DIR}/demuxstarted.txt ]]; then
+
+            # start with a clean slate: remove empty sample sheets before continuing
+            if [[ -e ${RUN_DIR}/SampleSheet.csv && ! -s ${RUN_DIR}/SampleSheet.csv  ]]; then
+                rm ${RUN_DIR}/SampleSheet.csv
+            fi
+
+            if [[ ! -e ${RUN_DIR}/SampleSheet.csv ]]; then
+                log "demux sheet fetch --application nova --pad --longest ${FC} > ${RUN_DIR}/SampleSheet.csv"
+                demux sheet fetch --application nova --pad --longest ${FC} > ${RUN_DIR}/SampleSheet.csv 
+            fi
+
+            # exit if samplesheet is still empty after running demux sheet fetch
+            if [[ -e ${RUN_DIR}/SampleSheet.csv && ! -s ${RUN_DIR}/SampleSheet.csv ]]; then
+                echo "Sample sheet empty! Exiting!" 1>&2
+                continue
+            fi
+
+            log "mkdir -p ${DEMUXES_DIR}/${RUN}/"
+            mkdir -p ${DEMUXES_DIR}/${RUN}/
+
+            log "date +'%Y%m%d%H%M%S' > ${RUN_DIR}/demuxstarted.txt"
+            date +'%Y%m%d%H%M%S' > ${RUN_DIR}/demuxstarted.txt
+
+            log "bash ${SCRIPT_DIR}/demux-novaseq.bash ${RUN_DIR} ${DEMUXES_DIR} &>> ${PROJECTLOG}"
+            bash ${SCRIPT_DIR}/demux-novaseq.bash ${RUN_DIR} ${DEMUXES_DIR} ${FC} ${PROJECTLOG} &>> ${PROJECTLOG}
+
+            if [[ $? == 0 ]]; then
+                log "rm -f ${DEMUXES_DIR}/${RUN}/copycomplete.txt"
+                rm -f ${DEMUXES_DIR}/${RUN}/copycomplete.txt
+                log "date +'%Y%m%d%H%M%S' > ${DEMUXES_DIR}/${RUN}/demuxcomplete.txt"
+                date +'%Y%m%d%H%M%S' > ${DEMUXES_DIR}/${RUN}/demuxcomplete.txt
+            fi
+
+            # This is an easy way to avoid running multiple demuxes at the same time
+            break
+        else
+            log "${RUN} is finished and demultiplexing has already started"
+        fi
+    else
+        log "${RUN} is not finished yet"
+    fi
+done
---
+++ scripts/novaseq/stage/demux-novaseq.bash
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+set -eu
+
+ulimit -n 4096
+
+##########
+# PARAMS #
+##########
+
+IN_DIR=${1?'please provide a run dir'}
+DEMUXES_DIR=${2?'please provide the demuxes dir'}
+FC=${3?'fc_id needed'}
+PROJECTLOG=${4?'projectlog needed'}
+
+RUN=$(basename ${IN_DIR})
+OUT_DIR=${DEMUXES_DIR}/${RUN}
+SCRIPT_DIR=/home/proj/${ENVIRONMENT}/bin/git/demultiplexing/scripts/novaseq/
+#EMAIL=clinical-demux@scilifelab.se
+EMAIL=your.name@scilifelab.se
+
+SLURM_ACCOUNT=development
+if [[ ${ENVIRONMENT} == 'production' ]]; then
+    SLURM_ACCOUNT=production
+fi
+
+#############
+# FUNCTIONS #
+#############
+
+log() {
+    local NOW=$(date +"%Y%m%d%H%M%S")
+    echo "[$NOW] $@"
+}
+
+########
+# MAIN #
+########
+
+# init
+mkdir -p ${OUT_DIR}
+
+# Here we go!
+log "Starting NovaSeq demultiplexing"
+# Send a mail that demultiplexing has started
+cat ${PROJECTLOG} | mail -s "Starting demultiplexing of novaseq flowcell ${FC} on $(hostname)" $EMAIL
+
+BASEMASK=$(demux basemask create --application nova ${IN_DIR})
+UNALIGNED_DIR=Unaligned-${BASEMASK//,}
+
+# DEMUX !
+JOB_TITLE=Demux_${RUN}
+log "sbatch --wait -A ${SLURM_ACCOUNT} -J ${JOB_TITLE} -o ${PROJECTLOG} ${SCRIPT_DIR}/demux-novaseq.batch ${IN_DIR} ${OUT_DIR} ${BASEMASK} ${UNALIGNED_DIR}"
+RES=$(sbatch --wait -A ${SLURM_ACCOUNT} -J ${JOB_TITLE} -o ${PROJECTLOG} ${SCRIPT_DIR}/demux-novaseq.batch ${IN_DIR} ${OUT_DIR} ${BASEMASK} ${UNALIGNED_DIR})
+
+log "bcl2fastq finished!"
+
+#add samplesheet to unaligned folder
+cp ${IN_DIR}/SampleSheet.csv ${OUT_DIR}/${UNALIGNED_DIR}/
+
+#Restructure the output dir!
+FC=${RUN##*_}
+FC=${FC:1}
+shopt -s nullglob
+for PROJECT_DIR in ${OUT_DIR}/${UNALIGNED_DIR}/*; do
+    if [[ ! -d ${PROJECT_DIR} ]]; then continue; fi
+
+    PROJECT=$(basename ${PROJECT_DIR})
+    if [[ ${PROJECT} == 'Stats' ]]; then continue; fi
+    if [[ ${PROJECT} == 'Reports' ]]; then continue; fi
+    if [[ ${PROJECT} =~ Project_* ]]; then continue; fi
+    if [[ ${PROJECT} == 'indexcheck' ]]; then
+        mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}
+        continue
+    fi
+
+    for SAMPLE_DIR in ${PROJECT_DIR}/*; do
+        for FASTQ_FILE in ${SAMPLE_DIR}/*; do
+            FASTQ=$(basename ${FASTQ_FILE})
+            log "mv ${FASTQ_FILE} ${SAMPLE_DIR}/${FC}_${FASTQ}"
+            mv ${FASTQ_FILE} ${SAMPLE_DIR}/${FC}_${FASTQ}
+        done
+
+        SAMPLE=$(basename ${SAMPLE_DIR})
+        log "mv ${SAMPLE_DIR} ${PROJECT_DIR}/Sample_${SAMPLE}"
+        mv ${SAMPLE_DIR} ${PROJECT_DIR}/Sample_${SAMPLE}
+    done
+
+    log "mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}"
+    mv ${PROJECT_DIR} ${OUT_DIR}/${UNALIGNED_DIR}/Project_${PROJECT}
+done
+
+#Add stats to cgstats database
+log "cgstats add --machine novaseq --unaligned ${UNALIGNED_DIR} ${OUT_DIR}"
+cgstats add --machine novaseq --unaligned ${UNALIGNED_DIR} ${OUT_DIR}
+
+PROJECTS=$(ls ${OUT_DIR}/${UNALIGNED_DIR}/ | grep Project_)
+for PROJECT_DIR in ${PROJECTS[@]}; do
+    PROJECT=${PROJECT_DIR##*_}
+    log "cgstats select --project ${PROJECT} ${FC} &> ${OUT_DIR}/stats-${PROJECT}-${FC}.txt"
+    cgstats select --project ${PROJECT} ${FC} &> ${OUT_DIR}/stats-${PROJECT}-${FC}.txt
+done
