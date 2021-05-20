58185c7cbbba5ed5fe8c0a98bc801c94da8c356c
Karl Nyren <kallejohan211@hotmail.com>
2021-05-20 15:23:38 +0200

    use the arithmic variable

--- scripts/hiseqx/xdemuxtiles.bash
+++ scripts/hiseqx/xdemuxtiles.bash
@@ -63,7 +63,7 @@ IS_DUAL=$(grep IndexRead2 "${RUNDIR}/runParameters.xml" | sed 's/<\/IndexRead2>\
 # calculate the number of lanes
 LANE_COUNT=$(awk '$0 ~/FlowcellLayout/ {split($0,arr," "); split(arr[2],o,"="); print o[2]}' "{$RUNDIR}/RunInfo.xml" | sed 's/\"//g')
 declare -a lanes=()
-for (( i=1; i<=2; i++ )); do
+for (( i=1; i<=LANE_COUNT; i++ )); do
   lanes[$i-1]=$i;
 done
 
